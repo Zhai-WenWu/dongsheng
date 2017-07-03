97436df21660e77a9eab4624aa05616a626d1445
ruijiao <1160676773@qq.com>
2017-07-03 15:26:29 +0800

    完善发布文章是否有多媒体资源判断

--- app/src/main/java/amodule/article/db/UploadParentSQLite.java
+++ app/src/main/java/amodule/article/db/UploadParentSQLite.java
@@ -257,8 +257,8 @@ public class UploadParentSQLite extends SQLiteOpenHelper {
             if (cur.moveToFirst()) {// 判断游标是否为空
                 content = cur.getString(cur.getColumnIndex(UploadArticleData.article_content));
             }
-            return !TextUtils.isEmpty(content) && (content.contains("\"type\":\"image\",\"imageurl\"")
-                    || content.contains("\"videourl\":") || content.contains("\"type\":\"gif\",\"gifurl\""));
+            return !TextUtils.isEmpty(content) && (content.contains("\"type\":\"image\"")
+                    || content.contains("\"type\":\"video\"") || content.contains("\"type\":\"gif\""));
         } finally {
             close(cur, readableDatabase);
         }
