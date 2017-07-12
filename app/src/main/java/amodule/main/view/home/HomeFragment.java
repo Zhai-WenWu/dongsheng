8e38cf2c84690f053619cf7881544e3fc6476af4
xiangha-zhangyujian <zhangyujian@xiangha.com>
2017-07-12 13:24:58 +0800

    修改每日三餐的统计字段

--- app/src/main/java/amodule/main/view/home/HomeFragment.java
+++ app/src/main/java/amodule/main/view/home/HomeFragment.java
@@ -186,7 +186,7 @@ public class HomeFragment extends BaseHomeFragment{
                 statisticKey = "other_top_list";
                 adPlayIds = AdPlayIdConfig.MAIN_HOME_ZHISHI_LIST;
             }else if("day".equals(type)){ //每日三餐
-                statisticKey = "other_threeMeals_list";
+                statisticKey = "sc_list";
                 isDayDish = true;
                 adPlayIds = AdPlayIdConfig.COMMEND_THREE_MEALS;
                 adIndexs = new Integer[]{};
@@ -438,6 +438,7 @@ public class HomeFragment extends BaseHomeFragment{
                         //当前只有向上刷新，并且服务端确认可以刷新数据
                         if (compelClearData || (refresh && !TextUtils.isEmpty(listmaps.get(0).get("reset")) && "2".equals(listmaps.get(0).get("reset")))) {
                             mListData.clear();
+                            Log.i("wyj","刷新数据：清集合");
                             isNeedRefresh(true);
                             //强制刷新，重置数据
                             if(!TextUtils.isEmpty(listmaps.get(0).get("backurl")))
