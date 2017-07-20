1eb7b1f24999dcfaa979557929bb5a1237efb0ca
mrtrying <ztanzeyu@gmail.com>
2017-07-20 09:23:18 +0800

    解决购物车背景问题 解决第三方登陆后，绑定手机和设置个人信息返回bug

--- app/src/main/java/amodule/user/activity/login/SetPersonalInfo.java
+++ app/src/main/java/amodule/user/activity/login/SetPersonalInfo.java
@@ -30,6 +30,7 @@ import acore.logic.XHClick;
 import acore.override.activity.base.BaseLoginActivity;
 import acore.tools.StringManager;
 import acore.tools.Tools;
+import amodule.main.Main;
 import amodule.user.view.NextStepView;
 import aplug.basic.InternetCallback;
 import aplug.basic.ReqInternet;
@@ -437,5 +438,11 @@ public class SetPersonalInfo extends BaseLoginActivity implements View.OnClickLi
         } else if (ORIGIN_REGISTER.equals(loginType)) {
             XHClick.mapStat(this, PHONE_TAG, "注册", threeLevel);
         }
+    }
+
+    @Override
+    public void finish() {
+        Main.colse_level = 4;
+        super.finish();
     }
 }
