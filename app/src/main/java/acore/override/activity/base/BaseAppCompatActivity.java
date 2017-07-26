5bca1939a49ff46725d4101a93cdb1d38e36510d
mrtrying <ztanzeyu@gmail.com>
2017-07-26 16:49:05 +0800

    添加新视频框架

---
+++ app/src/main/java/acore/override/activity/base/BaseAppCompatActivity.java
@@ -0,0 +1,231 @@
+package acore.override.activity.base;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.util.Util;
+import com.xiangha.R;
+
+import acore.logic.ActivityMethodManager;
+import acore.logic.load.LoadManager;
+import acore.override.XHApplication;
+import acore.tools.LogManager;
+import acore.tools.Tools;
+import acore.tools.ToolsDevice;
+import amodule.main.Main;
+import amodule.main.view.CommonBottomView;
+import amodule.main.view.CommonBottonControl;
+import third.share.BarShare;
+
+/**
+ * PackageName : acore.override.activity.base
+ * Created by MrTrying on 2017/7/26 15:26.
+ * E_mail : ztanzeyu@gmail.com
+ */
+
+public class BaseAppCompatActivity extends AppCompatActivity {
+    public RelativeLayout rl;
+    protected int level = 2;
+    public BarShare barShare;
+    public LoadManager loadManager = null;
+    public boolean keyBoard_visible = false;
+    protected ActivityMethodManager mActMagager;
+    public CommonBottomView mCommonBottomView;
+    public String className;
+    public CommonBottonControl control;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActMagager = new ActivityMethodManager(this);
+    }
+
+    /**
+     * Activity标题初始化
+     *
+     * @param title ：标题
+     * @param level ：等级
+     * @param color :背景色
+     * @param barTitleXml：标题xml
+     * @param contentXml：主内容xml
+     */
+    public void initActivity(String title, int level, int color, int barTitleXml, int contentXml) {
+        this.level = level;
+        className=this.getComponentName().getClassName();
+        control= new CommonBottonControl();
+        if (barTitleXml > 0) {
+            this.requestWindowFeature(Window.FEATURE_NO_TITLE); // 声明使用自定义标题
+            View view_all = LayoutInflater.from(this).inflate(R.layout.a_all,null);
+            RelativeLayout all_title= (RelativeLayout) view_all.findViewById(R.id.all_title);
+            RelativeLayout all_content= (RelativeLayout) view_all.findViewById(R.id.all_content);
+            all_content.addView(control.setCommonBottonView(className,this,contentXml));
+            View view_title = LayoutInflater.from(this).inflate(barTitleXml,null);
+//			if(Tools.isShowTitle()) {
+//				int dp_45 = Tools.getDimen(this, R.dimen.dp_45);
+//				int height = dp_45 + Tools.getStatusBarHeight(this);
+//				RelativeLayout.LayoutParams layout = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, height);
+//				all_title.setLayoutParams(layout);
+//				all_title.setPadding(0, Tools.getStatusBarHeight(this), 0, 0);
+//			}
+
+            all_title.addView(view_title);
+            setContentView(view_all);
+            mCommonBottomView=control.mCommonBottomView;
+            getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, barTitleXml);
+            TextView titleV = (TextView) findViewById(R.id.title);
+
+            titleV.setMaxWidth(ToolsDevice.getWindowPx(this).widthPixels - ToolsDevice.dp2px(this, 45 + 40));
+            titleV.setText(title);
+            if (color > 0)
+                findViewById(R.id.barTitle).setBackgroundResource(color);
+            // 设置返回按钮;
+            View btn_back = findViewById(R.id.ll_back);
+            ImageView img_back = (ImageView) findViewById(R.id.leftImgBtn);
+            TextView tv_back = (TextView) findViewById(R.id.leftText);
+            btn_back.setClickable(true);
+            tv_back.setClickable(true);
+            img_back.setClickable(true);
+            View.OnClickListener backClick = getBackBtnAction();
+            btn_back.setOnClickListener(backClick);
+            tv_back.setOnClickListener(backClick);
+            img_back.setOnClickListener(backClick);
+            btn_back.setVisibility(View.VISIBLE);
+            tv_back.setVisibility(View.INVISIBLE);
+            img_back.setVisibility(View.VISIBLE);
+        } else {
+            this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            setContentView(control.setCommonBottonView(className,this,contentXml));
+            mCommonBottomView=control.mCommonBottomView;
+        }
+        if(Tools.isShowTitle()) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        String colors = Tools.getColorStr(this, R.color.common_top_bg);
+        Tools.setStatusBarColor(this, Color.parseColor(colors));
+        setCommonStyle();
+    }
+
+    protected View.OnClickListener getBackBtnAction() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BaseAppCompatActivity.this.onBackPressed();
+            }
+        };
+    }
+
+    /**
+     * 设置公用属性
+     */
+    public void setCommonStyle() {
+        // 设置样式
+        rl = (RelativeLayout) findViewById(R.id.activityLayout);
+        if (rl != null) {
+            loadManager = new LoadManager(this, rl);
+            // 设置压缩模式下键盘的监听
+            rl.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                private int preHeight = 0;
+
+                @Override
+                public void onGlobalLayout() {
+                    int heightDiff = rl.getRootView().getHeight() - rl.getHeight();
+                    if (preHeight == heightDiff) {
+                        return;
+                    }
+                    preHeight = heightDiff;
+                    if (heightDiff > (ToolsDevice.getWindowPx(BaseAppCompatActivity.this).heightPixels / 4)) {
+                        if (!keyBoard_visible)
+                            keyBoard_visible = true;
+                    } else {
+                        if (keyBoard_visible)
+                            keyBoard_visible = false;
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return mActMagager.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        mActMagager.onMenuItemSelected(0, item);
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // 程序如果未初始化但却有定时器执行，则停止它。主要用于外部吊起应用时
+        if (Main.allMain == null && Main.timer != null) {
+            Main.stopTimer();
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (XHApplication.in() == null) {
+            Intent i = getBaseContext().getPackageManager()
+                    .getLaunchIntentForPackage(getBaseContext().getPackageName());
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+            return;
+        }
+        mActMagager.onResume(level);
+        if(mCommonBottomView!=null)
+            CommonBottomView.BottomViewBuilder.getInstance().refresh(mCommonBottomView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mActMagager.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mActMagager.onDestroy();
+        if(Util.isOnMainThread()) {
+            Glide.get(XHApplication.in()).clearMemory();
+            LogManager.print("d", "***********Glide is already clearMemory...");
+        }
+    }
+
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+        // 设置切换动画，从右边进入，左边退出
+        overridePendingTransition(R.anim.in_from_right, R.anim.out_to_left);
+    }
+
+    @Override
+    public void startActivityForResult(Intent intent, int requestCode) {
+        super.startActivityForResult(intent, requestCode);
+        // 设置切换动画，从右边进入，左边退出
+        overridePendingTransition(R.anim.in_from_right, R.anim.out_to_left);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.in_from_left, R.anim.out_to_right);
+    }
+}
--- app/src/main/java/amodule/other/activity/PlayVideo.java
+++ app/src/main/java/amodule/other/activity/PlayVideo.java
@@ -1,41 +1,33 @@
 package amodule.other.activity;
 
-import android.content.Context;
-import android.content.res.Configuration;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Environment;
-import android.text.TextUtils;
-import android.view.KeyEvent;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 
-import com.sina.sinavideo.sdk.VDVideoExtListeners;
-import com.sina.sinavideo.sdk.VDVideoView;
-import com.sina.sinavideo.sdk.data.VDVideoInfo;
 import com.xiangha.R;
 
-import acore.logic.XHClick;
-import acore.override.activity.base.BaseActivity;
-import acore.tools.CPUTool;
-import acore.tools.FileManager;
+import acore.override.activity.base.BaseAppCompatActivity;
 import acore.tools.Tools;
-import acore.tools.ToolsDevice;
-import third.video.VideoApplication;
+import fm.jiecao.jcvideoplayer_lib.JCMediaManager;
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayer;
+import fm.jiecao.jiecaovideoplayer.CustomView.MyJCVideoPlayerStandard;
 
-public class PlayVideo extends BaseActivity {
+public class PlayVideo extends BaseAppCompatActivity {
 
     private String url, img, name;
-    private LinearLayout dishVidioLayout;
-    private VDVideoView mVDVideoView;
+    private MyJCVideoPlayerStandard myJCVideoPlayerStandard;
+    boolean isBack = false;
+    boolean onceStart = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        getWindow().setFormat(PixelFormat.TRANSLUCENT);
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             name = bundle.getString("name");
@@ -46,151 +38,74 @@ public class PlayVideo extends BaseActivity {
             finish();
             return;
         }
-        initActivity(name, 2, 0, 0, R.layout.a_other_play_video);
+        initActivity(name,2,0,0,R.layout.a_other_play_video);
         init();
     }
 
     private void init() {
-//        initStatusBar();
-        findViewById(R.id.back).setOnClickListener(new OnClickListener() {
+        String colors = Tools.getColorStr(this, R.color.common_top_bg);
+        Tools.setStatusBarColor(this, Color.parseColor(colors));
+
+        findViewById(R.id.ll_back).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 PlayVideo.this.onBackPressed();
             }
         });
 
-        //设置播放界面16:9
-        dishVidioLayout = (LinearLayout) findViewById(R.id.video_layout);
-        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) dishVidioLayout.getLayoutParams();
-        params.height = Tools.getPhoneHeight();
-        params.width = Tools.getPhoneWidth();
-        dishVidioLayout.setLayoutParams(params);
         intiVideoView();
-        startPlay();
-    }
-
-    /**
-     * 初始化状态栏
-     */
-    private void initStatusBar() {
-        if (Tools.isShowTitle()) {
-            int dp_45 = Tools.getDimen(this, R.dimen.dp_45);
-            int height = dp_45 + Tools.getStatusBarHeight(this);
-            RelativeLayout bar_title = (RelativeLayout) findViewById(R.id.bar_title);
-            RelativeLayout.LayoutParams layout = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, height);
-            bar_title.setLayoutParams(layout);
-            bar_title.setPadding(0, Tools.getStatusBarHeight(this), 0, 0);
-        }
     }
 
     private void intiVideoView() {
-        if (ToolsDevice.isNetworkAvailable(this)) {
-            try {
-                VideoApplication.getInstence().initialize(this);
-            } catch (Exception e) {
-                statisticsInitVideoError(this);
-            }
-        }
-        mVDVideoView = new VDVideoView(this);
-        mVDVideoView.setLayers(R.array.my_videoview_layers);
-
-        mVDVideoView.setCompletionListener(new VDVideoExtListeners.OnVDVideoCompletionListener() {
-            @Override
-            public void onVDVideoCompletion(VDVideoInfo info, int status) {
-            }
-        });
-
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                RelativeLayout.LayoutParams.MATCH_PARENT,
-                RelativeLayout.LayoutParams.MATCH_PARENT);
-        dishVidioLayout.addView(mVDVideoView, layoutParams);
-        mVDVideoView.setVDVideoViewContainer(dishVidioLayout);
-    }
-
-    private void startPlay() {
-
-        if (!ToolsDevice.isNetworkAvailable(this)) {
-            Tools.showToast(this, "网络故障，请检查网络是否可用");
-            return;
-        }
-
-        if (TextUtils.isEmpty(url)) {
-            Tools.showToast(this, "视频地址为空");
-            return;
-        }
-
-        if (!url.startsWith("http")) {
-            Tools.showToast(this, "视频地址错误");
-            return;
-        }
-
-        if (VideoApplication.getInstence().isInitSuccess()) {
-            try {
-                VDVideoInfo videoInfo = new VDVideoInfo(url);
-                videoInfo.mTitle = "";
-                mVDVideoView.open(this, videoInfo);
-                mVDVideoView.play(0);
-            } catch (Exception e) {
-                Tools.showToast(this, "视频解码库加载失败，请重试");
-                FileManager.delDirectoryOrFile(Environment.getDataDirectory() + "/data/com.xiangha/libs/");
-                VideoApplication.getInstence().initialize(this);
-                return;
-            }
-        } else {
-            Tools.showToast(this, "加载视频解码库中...");
-            VideoApplication.getInstence().initialize(this);
-            return;
-        }
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (mVDVideoView != null
-                && !mVDVideoView.onVDKeyDown(keyCode, event)) {
-            return super.onKeyDown(keyCode, event);
-        }
-        return true;
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        if (mVDVideoView == null)
-            return;
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            mVDVideoView.setIsFullScreen(true);
-        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            mVDVideoView.setIsFullScreen(true);
-        }
+        myJCVideoPlayerStandard = ((MyJCVideoPlayerStandard) findViewById(R.id.jc_video));
+        myJCVideoPlayerStandard.setUp(url, JCVideoPlayer.SCREEN_LAYOUT_NORMAL, "");
+        JCVideoPlayer.FULLSCREEN_ORIENTATION = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+        JCVideoPlayer.NORMAL_ORIENTATION = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+        myJCVideoPlayerStandard.startVideo();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (mVDVideoView != null) {
-            mVDVideoView.onResume();
+        isBack = false;
+        if(!onceStart){
+            JCMediaManager.instance().mediaPlayer.start();
+            myJCVideoPlayerStandard.onStatePlaying();
         }
+        onceStart = false;
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (mVDVideoView != null) {
-            mVDVideoView.onPause();
+        if(isBack){
+            Log.i("tzy","PlayVideo releaseAllVideos");
+            JCVideoPlayer.releaseAllVideos();
+            JCVideoPlayer.clearSavedProgress(this, null);
+        }else{
+            Log.i("tzy","PlayVideo mediaPlayer onPause");
+            JCMediaManager.instance().mediaPlayer.pause();
+            myJCVideoPlayerStandard.onStatePause();
         }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (mVDVideoView != null) {
-            mVDVideoView.release(false);
-        }
     }
 
-    //统计视频初始化错误
-    private void statisticsInitVideoError(Context context) {
-        XHClick.mapStat(context, "init_video_error", "CPU型号", "" + CPUTool.getCpuName());
-        XHClick.mapStat(context, "init_video_error", "手机型号", android.os.Build.BRAND + "_" + android.os.Build.MODEL);
+    @Override
+    public void onBackPressed() {
+        if (JCVideoPlayer.backPress()) {
+            return;
+        }
+        isBack = true;
+        super.onBackPressed();
     }
 }
