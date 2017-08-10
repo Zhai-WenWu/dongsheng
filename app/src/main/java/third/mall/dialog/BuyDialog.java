368fa105d20fc8705f3bcad800bfbf3aef3206d2
xiangha-zhangyujian <zhangyujian@xiangha.com>
2017-08-10 17:05:32 +0800

    修改商品信息处理

--- app/src/main/java/third/mall/dialog/BuyDialog.java
+++ app/src/main/java/third/mall/dialog/BuyDialog.java
@@ -2,95 +2,137 @@ package third.mall.dialog;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 import com.xianghatest.R;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Map;
 
 import acore.tools.Tools;
 import aplug.basic.ReqInternet;
+import third.mall.activity.ShoppingActivity;
+import third.mall.activity.ShoppingOrderActivity;
 import third.mall.adapter.AdapterFavorable;
 import third.mall.aplug.MallInternetCallback;
 import third.mall.aplug.MallReqInternet;
 import third.mall.aplug.MallStringManager;
+import xh.basic.internet.UtilInternet;
 import xh.basic.tool.UtilString;
 
 /**
  * 选择优惠券的dialog
- * 
- * @author yujian
  *
+ * @author yujian
  */
 public class BuyDialog extends SimpleDialog {
 
-	private AdapterFavorable adapter;
-	private Context context;
-	private ArrayList<Map<String,String>> list_coupon = new ArrayList<Map<String,String>>();
-	private String shop_code;
-	private showCallBack callback;
-	public BuyDialog(Activity activity, String shop_code) {
-		super(activity, R.layout.dialog_mall_favorable);
-		this.context = activity;
-		this.shop_code= shop_code;
-		setLatyoutHeight();
-		init();
-		setCallBack(null);
-	}
+    private Context context;
+    private ImageView item_commod_iv;
+    private TextView item_commod_texts,item_commod_price,item_commod_num;
+    private RelativeLayout item_commod_cut;
+    private Map<String,String> mapData;
+    private int productNum=1;
 
-	public void init() {
-		ListView listview_dialog = (ListView) view.findViewById(R.id.listview_dialog);
-		adapter=new AdapterFavorable(context,listview_dialog, list_coupon, R.layout.item_mall_favorable_dialog, null, null);
-		listview_dialog.setAdapter(adapter);
-		setRequest(shop_code);
-	}
-	public void setCallBack(showCallBack callbacks){
-		if(null==callbacks){
-			callback= new showCallBack() {
-				
-				@Override
-				public void setShow() {
-					BuyDialog.this.show();
-				}
-			};
-		}else
-			this.callback= callbacks;
-	}
-	/**
-	 * 请求获取优惠券信息
-	 */
-	private void setRequest(String shop_code) {
-		String url=MallStringManager.mall_getShopCouponInfo+"?shop_code="+shop_code;
-		MallReqInternet.in().doGet(url, new MallInternetCallback(context) {
-			
-			@Override
-			public void loadstat(int flag, String url, Object msg, Object... stat) {
+    public BuyDialog(Activity activity, Map<String,String> mapData,int productNum) {
+        super(activity, R.layout.dialog_mall_buy);
+        this.context = activity;
+        this.mapData= mapData;
+        this.productNum= productNum;
+        setLatyoutHeight();
+        init();
+    }
 
-				list_coupon.clear();
-				if(flag>=ReqInternet.REQ_OK_STRING){
-					ArrayList<Map<String,String>> lists= UtilString.getListMapByJson(msg);
-					ArrayList<Map<String,String>> list_map= UtilString.getListMapByJson(lists.get(0).get("shop_coupon_package"));
-					for (int i = 0,size=list_map.size(); i < size; i++) {
-						list_coupon.add(list_map.get(i));
-					}
-					if(list_coupon.size()>0){
-						adapter.notifyDataSetChanged();
-						if(callback!=null){
-							callback.setShow();
-						}
-					}
-				}else if (flag==ReqInternet.REQ_CODE_ERROR && msg instanceof Map ){
-					Map<String,String> map=(Map<String, String>) msg;
-					Tools.showToast(context, map.get("msg"));
-				}
-			
-			}
-		});
-	}
+    public void init() {
+        item_commod_iv= (ImageView) findViewById(R.id.item_commod_iv);
+        item_commod_texts= (TextView) findViewById(R.id.item_commod_texts);
+        item_commod_price= (TextView) findViewById(R.id.item_commod_price);
+        item_commod_num= (TextView) findViewById(R.id.item_commod_num);
+        //减少数量
+        findViewById(R.id.item_commod_cut).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(productNum>1){
+                    --productNum;
+                    item_commod_num.setText(String.valueOf(productNum));
 
-	public interface showCallBack{
-		public void setShow();
-	}
+                }else{
+                    Tools.showToast(context,"最少购买一个");
+                }
+            }
+        });
+        //增加数量
+        findViewById(R.id.item_commod_add).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ++productNum;
+                item_commod_num.setText(String.valueOf(productNum));
+            }
+        });
+        //点击下一步
+        findViewById(R.id.next_order).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setRequestOrder(getOrderInfo());
+            }
+        });
+    }
+
+    private String getOrderInfo(){
+        try{
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("product_code",mapData.get("product_code"));
+            jsonObject.put("product_num",String.valueOf(productNum));
+            return  jsonObject.toString();
+        }catch (Exception e){
+        }
+        return "";
+    }
+    /**
+     * 生产订单
+     */
+    private void setRequestOrder(final String orderInfo) {
+        if(TextUtils.isEmpty(orderInfo)){
+            return;
+        }
+        String param="order_info="+orderInfo;
+        MallReqInternet.in().doPost(MallStringManager.mall_checkoutOrder_v2, param, new MallInternetCallback(context) {
+
+            @Override
+            public void loadstat(int flag, String url, Object msg, Object... stat) {
+                if(flag>= UtilInternet.REQ_OK_STRING){
+                    ArrayList<Map<String,String>> list_data=UtilString.getListMapByJson(UtilString.getListMapByJson(msg).get(0).get("sub_order"));
+                    if(list_data.size()>0){
+                        Intent intent = new Intent(context,ShoppingOrderActivity.class);
+                        intent.putExtra("msg_order", msg.toString());
+                        intent.putExtra("order_info", orderInfo);
+                        intent.putExtra("url", MallStringManager.mall_checkoutOrder_v2);
+                        if(stat!=null&&stat.length>0&& !TextUtils.isEmpty((String)stat[0])){
+                            intent.putExtra("stat", (String) stat[0]);
+                        }
+                    }else{
+                        setRequestOrder(orderInfo);
+                    }
+                }else if(flag==UtilInternet.REQ_CODE_ERROR && msg instanceof Map){
+                    @SuppressWarnings("unchecked")
+                    Map<String,String> map= (Map<String, String>) msg;
+                    if("6000008".equals(map.get("code"))){
+                        setRequestOrder(orderInfo);
+                    }else{
+                        Tools.showToast(context, map.get("msg")+"");
+                    }
+                }
+            }
+        });
+    }
+
 }
