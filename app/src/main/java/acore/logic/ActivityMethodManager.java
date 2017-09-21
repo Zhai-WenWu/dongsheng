adda3e4f49edbed7ffb1093b3953afafef73724b
mrtrying <ztanzeyu@gmail.com>
2017-09-21 21:12:18 +0800

    删除重复view

--- app/src/main/java/acore/logic/ActivityMethodManager.java
+++ app/src/main/java/acore/logic/ActivityMethodManager.java
@@ -4,13 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
+import com.popdialog.view.XHADView;
 import com.tencent.stat.StatService;
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.message.PushAgent;
-import com.xiangha.R;
 import com.xiangha.Welcome;
 
 import java.util.Map;
@@ -18,13 +16,11 @@ import java.util.Map;
 import acore.tools.FileManager;
 import acore.tools.StringManager;
 import acore.tools.Tools;
-import acore.widget.XHADView;
 import amodule.main.Main;
 import amodule.main.activity.MainHomePageNew;
 import amodule.other.listener.HomeKeyListener;
 import amodule.other.listener.HomeKeyListener.OnHomePressedListener;
 import aplug.basic.ReqEncyptInternet;
-import aplug.feedback.activity.Feedback;
 import third.ad.tools.WelcomeAdTools;
 import third.mall.aplug.MallCommon;
 import xh.basic.tool.UtilFile;
