b057d8d256f85c924326e7693d12ecfd14dd8a62
mrtrying <ztanzeyu@gmail.com>
2017-09-21 17:45:10 +0800

    添加默认数据非空判断

--- app/src/main/java/amodule/main/Main.java
+++ app/src/main/java/amodule/main/Main.java
@@ -159,16 +159,19 @@ public class Main extends Activity implements OnClickListener {
 
         // 当软件后台重启时,根据保存的值,回到关闭前状态的text的字体显示
         if (savedInstanceState != null) {
-            defaultTab = Integer.parseInt(savedInstanceState.getString("currentTab"));
-            if (defaultTab == 0 && mBuoy != null && !TextUtils.isEmpty(mBuoy.getFloatIndex()) && "1".equals(mBuoy.getFloatIndex())
-                    || defaultTab == 1 && mBuoy != null && !TextUtils.isEmpty(mBuoy.getFloatSubjectList()) && "1".equals(mBuoy.getFloatSubjectList())
-                    || defaultTab == 2
-                    || defaultTab == 3) {
-                if (mBuoy != null) {
-                    mBuoy.clearAnimation();
-                    mBuoy.hide();
-                    mBuoy.setClosed(true);
-                    mBuoy.setMove(true);
+            String currentTapStr = savedInstanceState.getString("currentTab");
+            if(!TextUtils.isEmpty(currentTapStr) && !"null".equals(currentTapStr)){
+                defaultTab = Integer.parseInt(savedInstanceState.getString("currentTab"));
+                if (defaultTab == 0 && mBuoy != null && !TextUtils.isEmpty(mBuoy.getFloatIndex()) && "1".equals(mBuoy.getFloatIndex())
+                        || defaultTab == 1 && mBuoy != null && !TextUtils.isEmpty(mBuoy.getFloatSubjectList()) && "1".equals(mBuoy.getFloatSubjectList())
+                        || defaultTab == 2
+                        || defaultTab == 3) {
+                    if (mBuoy != null) {
+                        mBuoy.clearAnimation();
+                        mBuoy.hide();
+                        mBuoy.setClosed(true);
+                        mBuoy.setMove(true);
+                    }
                 }
             }
         }
@@ -581,6 +584,7 @@ public class Main extends Activity implements OnClickListener {
     protected void onSaveInstanceState(Bundle outState) {
         /*try catch 住 super方法，尝试解决 IllegalStateException 异常*/
         try{
+            outState.putString("currentTab",""+defaultTab);
             super.onSaveInstanceState(outState);
         }catch (Exception ignored){}
     }
