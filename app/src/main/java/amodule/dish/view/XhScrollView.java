da2781dfed1c748c39b92cc06e23becb1442a0ab
SLL <shiliangliang@xiangha.com>
2017-10-18 20:02:49 +0800

    scrollview横向滑动问题

--- app/src/main/java/amodule/dish/view/XhScrollView.java
+++ app/src/main/java/amodule/dish/view/XhScrollView.java
@@ -105,6 +105,36 @@ public class XhScrollView extends ScrollView {
         }
     }
 
+    private float xDistance, yDistance, yLast, xLast;
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                xDistance = yDistance = 0f;
+                xLast = ev.getX();
+                yLast = ev.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                final float curX = ev.getX();
+                final float curY = ev.getY();
+
+                xDistance += Math.abs(curX - xLast);
+                yDistance += Math.abs(curY - yLast);
+                xLast = curX;
+                yLast = curY;
+
+                /**
+                 * X轴滑动距离大于Y轴滑动距离，也就是用户横向滑动时，返回false，ScrollView不处理这次事件，
+                 * 让子控件中的TouchEvent去处理，所以横向滑动的事件交由子控件处理， ScrollView只处理纵向滑动事件
+                 */
+                if (xDistance > yDistance) {
+                    return false;
+                }
+        }
+
+        return super.onInterceptTouchEvent(ev);
+    }
+
     /**
      *
      * 滚动的回调接口
