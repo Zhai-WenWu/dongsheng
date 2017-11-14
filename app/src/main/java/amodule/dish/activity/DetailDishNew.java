3e874ec48304874908da9700c8ed9ab7a3973e27
yujian129431 <740100799@qq.com>
2017-11-14 11:30:28 +0800

    修改菜谱详情页

---
+++ app/src/main/java/amodule/dish/activity/DetailDishNew.java
@@ -0,0 +1,39 @@
+package amodule.dish.activity;
+
+import android.os.Bundle;
+
+import acore.override.activity.base.BaseAppCompatActivity;
+
+/**
+ * 菜谱详情页原生标准
+ */
+public class DetailDishNew extends BaseAppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void refresh(){
+
+    }
+    public void setRequest(){
+
+    }
+    public void handleData(){
+
+    }
+}
