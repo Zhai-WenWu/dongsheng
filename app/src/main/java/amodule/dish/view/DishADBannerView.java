76338f66c24f2ef9c15e9822381ba4646d01c316
yujian129431 <740100799@qq.com>
2017-11-28 11:22:21 +0800

    菜谱详情页面--修改ui

--- app/src/main/java/amodule/dish/view/DishADBannerView.java
+++ app/src/main/java/amodule/dish/view/DishADBannerView.java
@@ -1,12 +1,9 @@
 package amodule.dish.view;
 
-import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 import com.xiangha.R;
 
@@ -16,13 +13,6 @@ import acore.logic.AppCommon;
 import acore.override.helper.XHActivityManager;
 import acore.override.view.ItemBaseView;
 import acore.tools.Tools;
-import third.ad.AdParent;
-import third.ad.AdsShow;
-import third.ad.BannerAd;
-import third.ad.TencenApiAd;
-import third.ad.tools.AdPlayIdConfig;
-
-import static third.ad.tools.TencenApiAdTools.TX_ID_DISH_DETAIL_BURDEN_TOP;
 
 /**
  * 用料上方广告处理——————api
@@ -41,13 +31,11 @@ public class DishADBannerView extends ItemBaseView {
     public DishADBannerView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr, R.layout.view_dish_ad);
     }
-
     @Override
     public void init() {
         super.init();img_banner= (ImageView) findViewById(R.id.img_banner);
     }
     public void setData(final Map<String,String> map){
-//        map.put("img","http://s1.cdn.xiangha.com/caipu/201609/0518/052119348809.jpg/NjQwX3J3MTcwN19jXzEtM18w");
         imgWidth=Tools.getPhoneWidth()-Tools.getDimen(context,R.dimen.dp_40);
         setViewImage(img_banner,map.get("img"));
         img_banner.setOnClickListener(new OnClickListener() {
@@ -56,19 +44,5 @@ public class DishADBannerView extends ItemBaseView {
                 AppCommon.openUrl(XHActivityManager.getInstance().getCurrentActivity(),map.get("appUrl"),true);
             }
         });
-
-    }
-
-    public void onListViewScroll() {
-//        int[] location = new int[2];
-//        if (ad_layout != null)
-//            ad_layout.getLocationOnScreen(location);
-//        adBurden.isOnScreen(location[1] > Tools.getStatusBarHeight(activity)
-//                && location[1] < Tools.getScreenHeight());
-//        Log.e("DishADView ","location "+location[1]);
-    }
-
-    public interface  AdCallBack{
-        public void getAdsShow(AdsShow adBurden);
     }
 }
