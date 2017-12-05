3ea15d4cfc78147a78416df45ae1086f3e442d74
mrtrying <tanzeyu@xiangha.com>
2017-12-05 10:37:14 +0800

    处理icon显示逻辑

--- app/src/main/java/amodule/home/view/HomeActivityIconView.java
+++ app/src/main/java/amodule/home/view/HomeActivityIconView.java
@@ -1,7 +1,6 @@
 package amodule.home.view;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatImageView;
 import android.text.TextUtils;
@@ -9,20 +8,13 @@ import android.util.AttributeSet;
 import android.util.Log;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.animation.GlideAnimation;
-import com.bumptech.glide.request.target.Target;
 import com.xiangha.R;
 
 import java.util.Map;
 
 import acore.tools.StringManager;
-import acore.tools.Tools;
-import aplug.basic.SubBitmapTarget;
 import third.ad.tools.AdConfigTools;
 import third.ad.tools.AdPlayIdConfig;
-import xh.basic.internet.img.BitmapTarget;
 
 /**
  * Description :
@@ -60,8 +52,6 @@ public class HomeActivityIconView extends AppCompatImageView {
 
     private void initData(){
         setImageResource(R.drawable.home_default_icon);
-        int padding = Tools.getDimen(getContext(), R.dimen.dp_10);
-        setPadding(padding,0,padding,0);
     }
 
     //获取数据
@@ -95,20 +85,6 @@ public class HomeActivityIconView extends AppCompatImageView {
         this.mUrl = dataMap.get("url");
         Glide.with(getContext())
                 .load(imgUrl)
-                .listener(new RequestListener<String, GlideDrawable>() {
-                    @Override
-                    public boolean onException(Exception e, String s, Target<GlideDrawable> target, boolean b) {
-                        return false;
-                    }
-
-                    @Override
-                    public boolean onResourceReady(GlideDrawable glideDrawable, String s, Target<GlideDrawable> target, boolean b, boolean b1) {
-                        int padding_20 = Tools.getDimen(getContext(), R.dimen.dp_20);
-                        int padding_15 = Tools.getDimen(getContext(), R.dimen.dp_15);
-                        setPadding(padding_20,0,padding_15,0);
-                        return false;
-                    }
-                })
                 .into(this);
     }
 
