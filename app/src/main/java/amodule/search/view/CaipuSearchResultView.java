4a6e57d6de97c833b2ef8b68df00454f926c4cc5
guoyule <guoyule@xiangha.com>
2017-12-12 11:23:04 +0800

    替换文字

--- app/src/main/java/amodule/search/view/CaipuSearchResultView.java
+++ app/src/main/java/amodule/search/view/CaipuSearchResultView.java
@@ -417,7 +417,7 @@ public class CaipuSearchResultView extends LinearLayout {
     private void setLoadMoreBtn() {
         Button moreBtn = loadManager.getSingleLoadMore(list_search_result);
         moreBtn.setEnabled(false);
-        moreBtn.setText("— 学名厨做菜 —");
+        moreBtn.setText("- 学名厨做菜, 用香哈 -");
     }
 
 
