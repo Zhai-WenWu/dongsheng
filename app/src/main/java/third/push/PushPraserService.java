811b751f2eaac0f4141218f49ef67e4d64c8ebe1
mrtrying <tanzeyu@xiangha.com>
2017-12-22 18:37:27 +0800

    处理推送重复接收问题

--- app/src/main/java/third/push/PushPraserService.java
+++ app/src/main/java/third/push/PushPraserService.java
@@ -70,6 +70,14 @@ public class PushPraserService extends Service{
 		ArrayList<Map<String, String>> msgt = StringManager.getListMapByJson(extrajson);
 		if (msgt.size() > 0) {
 			Map<String, String> msgMap = msgt.get(0);
+			//用于解决：服务端会两个推送都推，根据pushCode存储本地情况，判断是否已经接受了推送
+			if (FileManager.ifFileModifyByCompletePath(FileManager.getDataDir() + msgMap.get("pushCode"), -1) != null) {
+//				XHClick.onEvent(mContext, PUSH_ID, type, "2");
+				return;
+			} else {
+//				XHClick.onEvent(mContext, PUSH_ID, type, "1");
+				FileManager.saveFileToCompletePath(FileManager.getDataDir() + msgMap.get("pushCode"), "", false);
+			}
 			//创建NotificationData
 			NotificationData data = new NotificationData();
 			data.setContent(msgAlert);
