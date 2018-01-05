06bcbd9569c3deb4fb35546bcdedbce3384b36f4
SLL <shiliangliang@xiangha.com>
2018-01-05 21:49:00 +0800

    分享回调参数调整

--- app/src/main/java/aplug/web/FullScreenWeb.java
+++ app/src/main/java/aplug/web/FullScreenWeb.java
@@ -116,8 +116,8 @@ public class FullScreenWeb extends WebActivity implements IObserver {
                 case ObserverManager.NOTIFY_SHARE:
                     if (!TextUtils.isEmpty(shareCallback) && data != null) {
                         Map<String, String> dataMap = (Map<String, String>) data;
-                        webview.loadUrl("javascript:" + shareCallback + "(\"" + dataMap.get("status") + "\")");
-                        Log.i("tzy", "javascript:" + shareCallback + "(\"" + dataMap.get("status") + "\")");
+                        webview.loadUrl("javascript:" + shareCallback + "(" + TextUtils.equals("2", dataMap.get("status")) + ")");
+                        Log.i("tzy", "javascript:" + shareCallback + "(" + TextUtils.equals("2", dataMap.get("status")) + ")");
                     }
                     break;
                 case ObserverManager.NOTIFY_YIYUAN_BIND:
--- app/src/main/java/aplug/web/ShowWeb.java
+++ app/src/main/java/aplug/web/ShowWeb.java
@@ -395,8 +395,8 @@ public class  ShowWeb extends WebActivity implements IObserver {
 			case ObserverManager.NOTIFY_SHARE:
 				if (!TextUtils.isEmpty(shareCallback) && data != null) {
 					Map<String, String> dataMap = (Map<String, String>) data;
-					webview.loadUrl("javascript:" + shareCallback + "(\"" + dataMap.get("status") + "\")");
-					Log.i("tzy", "javascript:" + shareCallback + "(\"" + dataMap.get("status") + "\")");
+					webview.loadUrl("javascript:" + shareCallback + "(" + TextUtils.equals("2", dataMap.get("status")) + ")");
+					Log.i("tzy", "javascript:" + shareCallback + "(" + TextUtils.equals("2", dataMap.get("status")) + ")");
 				}
 				break;
 			case ObserverManager.NOTIFY_LOGIN:
