824eb87e17284c866687d50aca4b040fea0ff6be
SLL <shiliangliang@xiangha.com>
2018-01-08 16:18:59 +0800

    打开系统各设置界面工具类

---
+++ app/src/main/java/acore/tools/SettingUtils.java
@@ -0,0 +1,38 @@
+package acore.tools;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+
+import acore.override.XHApplication;
+
+/**
+ * Created by sll on 2018/1/8.
+ */
+
+public class SettingUtils {
+
+    public static class NotificationSetting {
+
+
+
+    }
+
+    public static class ApplicationSetting {
+
+        public static void openApplicationDetailSettings() {
+            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,getPacageUri());
+            XHApplication.in().getApplicationContext().startActivity(intent);
+        }
+
+    }
+
+    public static void openSystemSettings() {
+        Intent intent = new Intent(Settings.ACTION_SETTINGS);
+        XHApplication.in().getApplicationContext().startActivity(intent);
+    }
+
+    private static Uri getPacageUri() {
+        return Uri.parse("package:" + XHApplication.in().getPackageName());
+    }
+}
