635bbb358245f737185ff4ea22201dcd1dcedd0c
mrtrying <tanzeyu@xiangha.com>
2018-02-05 11:45:38 +0800

    添加非空判断

--- app/src/main/java/amodule/main/Main.java
+++ app/src/main/java/amodule/main/Main.java
@@ -379,7 +379,10 @@ public class Main extends Activity implements OnClickListener, IObserver, ISetMe
         }
         isForeground = true;
         //去我的页面
-        if (MallPayActivity.pay_state) {
+        if (MallPayActivity.pay_state
+                && tabViews != null
+                && tabViews.length >= classes.length - 1
+                && tabViews[classes.length - 1] != null) {
             onClick(tabViews[classes.length - 1].findViewById(R.id.tab_layout));
         }
         //去商城页面
