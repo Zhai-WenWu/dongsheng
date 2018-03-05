b0680ce874088f19417f36db93fcae2117535c9b
mrtrying <tanzeyu@xiangha.com>
2018-03-05 16:07:22 +0800

    处理home监听注册问题

--- app/src/main/java/amodule/other/listener/HomeKeyListener.java
+++ app/src/main/java/amodule/other/listener/HomeKeyListener.java
@@ -6,17 +6,17 @@ import android.content.Intent;
 import android.content.IntentFilter;
 
 public class HomeKeyListener {
-	static final String TAG = "HomeListener"; 
-    private Context mContext; 
+    private Context mContext;
     private IntentFilter mFilter; 
     private OnHomePressedListener mListener; 
-    private InnerRecevier mRecevier; 
+    private InnerRecevier mRecevier;
+    private boolean isWatching = false;
  
     // 回调接口  
     public interface OnHomePressedListener { 
-        public void onHomePressed(); 
+        void onHomePressed();
  
-        public void onHomeLongPressed(); 
+        void onHomeLongPressed();
     } 
  
     public HomeKeyListener(Context context) { 
@@ -27,7 +27,7 @@ public class HomeKeyListener {
     /**
      * 设置监听
      * 
-     * @param listener
+     * @param listener 监听
      */ 
     public void setOnHomePressedListener(OnHomePressedListener listener) { 
         mListener = listener; 
@@ -38,7 +38,8 @@ public class HomeKeyListener {
      * 开始监听，注册广播
      */ 
     public void startWatch() { 
-        if (mRecevier != null) {
+        if (mRecevier != null && !isWatching) {
+            isWatching = true;
             try {
                 mContext.registerReceiver(mRecevier, mFilter);
             }catch (Exception e){e.printStackTrace();}
@@ -49,21 +50,22 @@ public class HomeKeyListener {
      * 停止监听，注销广播
      */ 
     public void stopWatch() { 
-        if (mRecevier != null) { 
+        if (mRecevier != null && isWatching) {
+            isWatching = false;
             mContext.unregisterReceiver(mRecevier); 
         } 
     }
     
     class InnerRecevier extends BroadcastReceiver{
     	final String SYSTEM_DIALOG_REASON_KEY = "reason"; 
-        final String SYSTEM_DIALOG_REASON_GLOBAL_ACTIONS = "globalactions"; 
-        final String SYSTEM_DIALOG_REASON_RECENT_APPS = "recentapps"; 
+        final String SYSTEM_DIALOG_REASON_RECENT_APPS = "recentapps";
         final String SYSTEM_DIALOG_REASON_HOME_KEY = "homekey"; 
 
         @Override 
         public void onReceive(Context context, Intent intent) { 
-            String action = intent.getAction(); 
-            if (action.equals(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)) { 
+            String action = intent.getAction();
+            if (action != null
+                    && action.equals(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)) {
                 String reason = intent.getStringExtra(SYSTEM_DIALOG_REASON_KEY); 
                 if (reason != null) { 
 //                    Log.e(TAG, "action:" + action + ",reason:" + reason); 
