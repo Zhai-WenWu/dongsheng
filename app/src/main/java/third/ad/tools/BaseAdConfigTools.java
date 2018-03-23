1cda6a26a58191042d77412a8c449867844add23
mrtrying <tanzeyu@xiangha,com>
2018-03-23 13:50:08 +0800

    处理广告上报字段

--- app/src/main/java/third/ad/tools/BaseAdConfigTools.java
+++ app/src/main/java/third/ad/tools/BaseAdConfigTools.java
@@ -75,25 +75,25 @@ public class BaseAdConfigTools {
 
     /**
      * @param event：行为事件
-     * @param gg_position_id：广告位id
-     * @param gg_position_code：广告商id or 自由广告position_id
+     * @param gg_position_id：广告商id or 自由广告position_id
+     * @param gg_position_code：广告位id
      * @param gg_business：广告商
      * @param gg_business_id：自由广告id（只有自有才有该字段）
      */
-    public void postStatistics(@NonNull String event, @NonNull String gg_position_id,@NonNull String gg_position_code,
+    public void postStatistics(@NonNull String event, @NonNull String gg_position_code,@NonNull String gg_position_id,
                                @NonNull String gg_business, @NonNull String gg_business_id) {
         LinkedHashMap<String, String> map = new LinkedHashMap<>();
         //时间
         map.put("app_time", Tools.getAssignTime("yyyy-MM-dd HH:mm:ss", 0));
         //行为事件
         map.put("event", event);
-        //广告位id
-        if (!TextUtils.isEmpty(gg_position_id)) {
-            map.put("gg_position_id", gg_position_id);
-        }
         //广告位code
         if (!TextUtils.isEmpty(gg_position_code)) {
             map.put("gg_position_code", gg_position_code);
+        }
+        //广告位id
+        if (!TextUtils.isEmpty(gg_position_id)) {
+            map.put("gg_position_id", gg_position_id);
         }
         //广告商
         if (!TextUtils.isEmpty(gg_business)) {
