2069d175fe1ee602b178b74a2d45358af3e09971
yujian <740100799@qq.com>
2018-09-06 17:22:15 +0800

    按要求重新矫正ui

---
+++ app/src/main/java/third/aliyun/views/MediaSelectView.java
@@ -0,0 +1,169 @@
+package third.aliyun.views;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.aliyun.jasonparse.JSONSupportImpl;
+import com.xh.manager.DialogManager;
+import com.xh.manager.ViewManager;
+import com.xh.view.HButtonView;
+import com.xh.view.TitleView;
+import com.xiangha.R;
+
+import third.aliyun.media.GalleryDirChooser;
+import third.aliyun.media.GalleryMediaChooser;
+import third.aliyun.media.MediaDir;
+import third.aliyun.media.MediaInfo;
+import third.aliyun.media.MediaStorage;
+import third.aliyun.media.ThumbnailGenerator;
+
+/**
+ * Description :
+ * PackageName : third.aliyun.views
+ * Created by mrtrying on 2018/8/13 15:08.
+ * e_mail : ztanzeyu@gmail.com
+ */
+public class MediaSelectView extends RelativeLayout {
+    private RelativeLayout topbar;
+    private TextView title, rightText,closeText;
+    private ImageView close;
+    private RecyclerView galleryView;
+
+    private MediaStorage storage;
+    private GalleryDirChooser galleryDirChooser;
+    private ThumbnailGenerator thumbnailGenerator;
+    private GalleryMediaChooser galleryMediaChooser;
+
+    private OnCloseClickCallback onCloseClickCallback;
+    private OnSelectMediaCallback onSelectMediaCallback;
+
+    public MediaSelectView(Context context) {
+        this(context, null);
+    }
+
+    public MediaSelectView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MediaSelectView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initialize();
+    }
+
+    private void initialize() {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.work_aliyun_svideo_import_view_media, null);
+        addView(view,LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT);
+        initializeUI();
+        initializeData();
+    }
+
+    private void initializeUI() {
+        topbar = findViewById(R.id.gallery_actionBar_rela);
+        title = findViewById(R.id.gallery_title);
+        rightText = findViewById(R.id.gallery_right_text);
+        close = findViewById(R.id.close);
+        closeText = findViewById(R.id.close_text);
+        galleryView = findViewById(R.id.gallery_media);
+        //设置数据和listener
+        title.setText(R.string.gallery_all_media);
+        close.setOnClickListener(this::handleCloseCallback);
+        closeText.setOnClickListener(this::handleCloseCallback);
+    }
+
+    private void initializeData() {
+        storage = new MediaStorage(getContext(), new JSONSupportImpl());
+        storage.setSortMode(MediaStorage.SORT_MODE_VIDEO);
+        storage.startFetchmedias();
+        storage.setOnMediaDirChangeListener(() -> {
+            MediaDir dir = storage.getCurrentDir();
+            title.setText(dir.id != -1 ? dir.dirName : getContext().getString(R.string.gallery_all_media));
+            galleryMediaChooser.changeMediaDir(dir);
+        });
+        storage.setOnCurrentMediaInfoChangeListener(new MediaStorage.OnCurrentMediaInfoChange() {
+            @Override
+            public void onCurrentMediaInfoChanged(MediaInfo info) {
+                if (info.duration < 3000) {
+                    dialogShow();
+                    return;
+                }
+                handleSelectMediaCallback(info);
+            }
+        });
+
+        thumbnailGenerator = new ThumbnailGenerator(getContext());
+        galleryDirChooser = new GalleryDirChooser(getContext(), findViewById(R.id.topPanel), thumbnailGenerator, storage);
+        galleryMediaChooser = new GalleryMediaChooser(galleryView, galleryDirChooser, storage, thumbnailGenerator);
+    }
+
+
+    private void dialogShow() {
+        DialogManager dialogManager = new DialogManager(getContext());
+        dialogManager.createDialog(new ViewManager(dialogManager)
+                .setView(new TitleView(getContext()).setText("请选择大于3秒的视频"))
+                .setView(new HButtonView(getContext())
+                        .setNegativeText("确定", v -> dialogManager.cancel())))
+                .show();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        destroy();
+    }
+
+    public void destroy() {
+        if (storage != null) {
+            storage.saveCurrentDirToCache();
+            storage.cancelTask();
+        }
+        if (thumbnailGenerator != null) {
+            thumbnailGenerator.cancelAllTask();
+        }
+    }
+
+    private void handleCloseCallback(View v) {
+        if (onCloseClickCallback != null) {
+            onCloseClickCallback.onCloseClick(v);
+        }
+    }
+
+    private void handleSelectMediaCallback(MediaInfo info) {
+        if (onSelectMediaCallback != null) {
+            onSelectMediaCallback.onSelectMedia(info);
+        }
+    }
+
+    public void setRightText(String text) {
+        rightText.setText(TextUtils.isEmpty(text) ? "" : text);
+        rightText.setVisibility(TextUtils.isEmpty(text)?GONE:VISIBLE);
+    }
+
+    public void setOnRightClickListener(OnClickListener listener){
+        if(listener != null){
+            rightText.setOnClickListener(listener);
+        }
+    }
+
+    public void setOnCloseClickCallback(OnCloseClickCallback onCloseClickCallback) {
+        this.onCloseClickCallback = onCloseClickCallback;
+    }
+
+    public void setOnSelectMediaCallback(OnSelectMediaCallback onSelectMediaCallback) {
+        this.onSelectMediaCallback = onSelectMediaCallback;
+    }
+
+    public interface OnCloseClickCallback {
+        void onCloseClick(View view);
+    }
+
+    public interface OnSelectMediaCallback {
+        void onSelectMedia(MediaInfo info);
+    }
+}
--- app/src/main/java/third/aliyun/work/MediaActivity.java
+++ app/src/main/java/third/aliyun/work/MediaActivity.java
@@ -4,66 +4,41 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
-import android.util.Log;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
-import com.aliyun.jasonparse.JSONSupportImpl;
-import com.aliyun.qupai.import_core.AliyunIImport;
 import com.aliyun.struct.common.AliyunVideoParam;
 import com.aliyun.struct.common.CropKey;
 import com.aliyun.struct.common.ScaleMode;
 import com.aliyun.struct.common.VideoQuality;
-import com.xh.manager.DialogManager;
-import com.xh.manager.ViewManager;
-import com.xh.view.HButtonView;
-import com.xh.view.TitleView;
+import com.quze.videorecordlib.media.MediaInfo;
 import com.xiangha.R;
 
 import acore.tools.Tools;
 import third.aliyun.edit.util.Common;
-import third.aliyun.media.GalleryDirChooser;
-import third.aliyun.media.GalleryMediaChooser;
-import third.aliyun.media.MediaDir;
-import third.aliyun.media.MediaInfo;
-import third.aliyun.media.MediaStorage;
-import third.aliyun.media.ThumbnailGenerator;
+import third.aliyun.views.MediaSelectView;
 
 /**
  * 视频选择
  */
-public class MediaActivity extends Activity implements View.OnClickListener {
-    public static final int[][] resolutions = new int[][]{new int[]{540, 720}, new int[]{540, 540}, new int[]{540, 960}};
+public class MediaActivity extends Activity {
+    public static final int[][] resolutions = new int[][]{new int[]{720, 960}, new int[]{720, 720}, new int[]{720, 1280}};
     private static final int REQUEST_CODE_VIDEO_CROP = 1;
     private static final int REQUEST_CODE_IMAGE_CROP = 2;
-    private MediaStorage storage;
-    private GalleryDirChooser galleryDirChooser;
-    private ThumbnailGenerator thumbnailGenerator;
-    private GalleryMediaChooser galleryMediaChooser;
-    private RecyclerView galleryView;
-    private TextView title;
     private int mRatio;
     private ScaleMode scaleMode = ScaleMode.LB;
     private int frameRate;
     private int gop;
     private int mBitrate;
     private VideoQuality quality = VideoQuality.SSD;
-    private AliyunIImport mImport;
     private MediaInfo mCurrMediaInfo;
     private int mCropPosition;
     private boolean mIsReachedMaxDuration = false;
     private AliyunVideoParam mVideoParam;
     private int[] mOutputResolution = null;
 
-    private int requestWidth;
-    private int requestHeight;
-    private ImageView close;
-    private RelativeLayout gallery_actionBar_rela;
+    private int requestWidth,requestHeight;
+    private MediaSelectView mediaSelectView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -73,9 +48,8 @@ public class MediaActivity extends Activity implements View.OnClickListener {
         getData();
         Tools.setStatusBarTrans(this);
         init();
-//        Common.requestMusic();
+        Common.requestMusic();
         AliyunCommon.getInstance().addActivity(this);
-        Log.i("xianghaTag","MediaActivity::::111111");
     }
 
     /**
@@ -117,99 +91,37 @@ public class MediaActivity extends Activity implements View.OnClickListener {
     }
 
     private void init() {
-        galleryView = (RecyclerView) findViewById(R.id.gallery_media);
-        title = (TextView) findViewById(R.id.gallery_title);
-        title.setText(R.string.gallery_all_media);
-        close= (ImageView) findViewById(R.id.close);
-        close.setOnClickListener(this);
-        storage = new MediaStorage(this, new JSONSupportImpl());
-        thumbnailGenerator = new ThumbnailGenerator(this);
-        galleryDirChooser = new GalleryDirChooser(this, findViewById(R.id.topPanel), thumbnailGenerator, storage);
-        galleryMediaChooser = new GalleryMediaChooser(galleryView, galleryDirChooser, storage, thumbnailGenerator);
-        storage.setSortMode(MediaStorage.SORT_MODE_VIDEO);
-        storage.startFetchmedias();
-        storage.setOnMediaDirChangeListener(new MediaStorage.OnMediaDirChange() {
-            @Override
-            public void onMediaDirChanged() {
-                MediaDir dir = storage.getCurrentDir();
-                if (dir.id == -1) {
-                    title.setText(getString(R.string.gallery_all_media));
-                } else {
-                    title.setText(dir.dirName);
-                }
-                galleryMediaChooser.changeMediaDir(dir);
-            }
+        mediaSelectView = findViewById(R.id.mediaSelectView);
+        mediaSelectView.setOnCloseClickCallback(view -> onBackPressed());
+//        mediaSelectView.setOnRightClickListener(v -> VideoDraftActivity.startActivity(MediaActivity.this));
+        mediaSelectView.setOnSelectMediaCallback(info -> {
+            //跳转裁剪
+            Intent intent = new Intent(MediaActivity.this, AliyunVideoCrop.class);
+            intent.putExtra(CropKey.VIDEO_PATH, info.filePath);
+            intent.putExtra(CropKey.VIDEO_DURATION, info.duration);
+            intent.putExtra("videoInfo",info);
+            intent.putExtra(CropKey.VIDEO_RATIO, mRatio);
+            intent.putExtra(CropKey.VIDEO_SCALE, scaleMode);
+            intent.putExtra(CropKey.VIDEO_QUALITY, quality);
+            intent.putExtra(CropKey.VIDEO_GOP, gop);
+            intent.putExtra(CropKey.VIDEO_BITRATE, mBitrate);
+            intent.putExtra(CropKey.VIDEO_FRAMERATE, frameRate);
+            intent.putExtra(CropKey.ACTION, CropKey.ACTION_TRANSCODE);//是否真裁剪
+            startActivityForResult(intent, REQUEST_CODE_VIDEO_CROP);
         });
-        storage.setOnCurrentMediaInfoChangeListener(new MediaStorage.OnCurrentMediaInfoChange() {
-            @Override
-            public void onCurrentMediaInfoChanged(MediaInfo info) {
-                //到裁剪页面
-                if(info.duration<3000){
-                    dialogShow();
-                    return;
-                }
-                Intent intent = new Intent(MediaActivity.this, AliyunVideoCrop.class);
-                intent.putExtra(CropKey.VIDEO_PATH, info.filePath);
-                intent.putExtra(CropKey.VIDEO_DURATION, info.duration);
-                intent.putExtra("videoInfo",info);
-                intent.putExtra(CropKey.VIDEO_RATIO, mRatio);
-                intent.putExtra(CropKey.VIDEO_SCALE, scaleMode);
-                intent.putExtra(CropKey.VIDEO_QUALITY, quality);
-                intent.putExtra(CropKey.VIDEO_GOP, gop);
-                intent.putExtra(CropKey.VIDEO_BITRATE, mBitrate);
-                intent.putExtra(CropKey.VIDEO_FRAMERATE, frameRate);
-                intent.putExtra(CropKey.ACTION, CropKey.ACTION_TRANSCODE);//是否真裁剪
-                startActivityForResult(intent, REQUEST_CODE_VIDEO_CROP);
-
-
-            }
-        });
-
-        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.Callback() {
-            @Override
-            public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
-                //首先回调的方法 返回int表示是否监听该方向
-                int dragFlags = ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;//拖拽
-                int swipeFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;//侧滑删除
-                return makeMovementFlags(dragFlags, swipeFlags);
-            }
-
-            @Override
-            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
-                //滑动事件
-//                mTransCoder.swap(viewHolder.getAdapterPosition(), target.getAdapterPosition());
-                return false;
-            }
-
-            @Override
-            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-            }
-
-            @Override
-            public boolean isItemViewSwipeEnabled() {
-                return false;
-            }
-
-            @Override
-            public boolean isLongPressDragEnabled() {
-                //是否可拖拽
-                return true;
-            }
-        });
-        gallery_actionBar_rela= (RelativeLayout) findViewById(R.id.gallery_actionBar_rela);
         initTitle();
     }
-    private void initTitle(){
-        if (Tools.isShowTitle()) {
-            int height = Tools.getStatusBarHeight(this);
-            gallery_actionBar_rela.setPadding(0, height, 0, 0);
-        }
+
+    private void updateDraftText() {
+//        int draftSize = PublishDraftDBHelper.getInstance(this).draftSize();
+//        mediaSelectView.setRightText(draftSize > 0 ? "草稿箱("+draftSize +")":"");
     }
-    private void dialogShow(){
-        DialogManager dialogManager = new DialogManager(this);
-        dialogManager.createDialog(new ViewManager(dialogManager)
-        .setView(new TitleView(this).setText("请选择大于3秒的视频"))
-        .setView(new HButtonView(this).setNegativeText("确定", v -> dialogManager.cancel()))).show();
+
+    private void initTitle(){
+//        if (Tools.isShowTitle()) {
+//            LinearLayout rootLayout = findViewById(R.id.rootLayout);
+//            rootLayout.setPadding(0, Tools.getStatusBarHeight(this), 0, 0);
+//        }
     }
 
     @Override
@@ -253,20 +165,18 @@ public class MediaActivity extends Activity implements View.OnClickListener {
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        storage.saveCurrentDirToCache();
-        storage.cancelTask();
-        thumbnailGenerator.cancelAllTask();
+    protected void onResume() {
+        super.onResume();
+        updateDraftText();
     }
 
     @Override
-    public void onClick(View v) {
-        if (v == close) {
-            finish();
-        }
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaSelectView.destroy();
     }
-//    private EditText frameRateEdit//帧率
+
+    //    private EditText frameRateEdit//帧率
 //            , gopEdit//关键帧间隔
 //            , mBitrateEdit;//码率
 //    private ImageView back;
