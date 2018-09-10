21861a64bc44125db4494b3fab09ea849d0a93b8
zeyu_tan <306651457@qq.com>
2018-09-10 15:41:13 +0800

    个人中心列表ui 视频数据库处理

--- app/src/main/java/amodule/user/view/UserHomeVideo.java
+++ app/src/main/java/amodule/user/view/UserHomeVideo.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.xh.manager.DialogManager;
 import com.xh.manager.ViewManager;
@@ -33,11 +34,11 @@ import acore.logic.load.LoadManager;
 import acore.override.activity.base.BaseLoginActivity;
 import acore.tools.StringManager;
 import acore.tools.Tools;
-import acore.widget.DownRefreshList;
 import amodule.article.activity.edit.VideoEditActivity;
 import amodule.article.db.UploadArticleData;
 import amodule.article.db.UploadVideoSQLite;
 import amodule.dish.db.UploadDishData;
+import amodule.shortvideo.tools.ShortVideoPublishManager;
 import amodule.user.activity.FriendHome;
 import amodule.user.adapter.AdapterUserVideo;
 import aplug.basic.InternetCallback;
@@ -83,6 +84,22 @@ public class UserHomeVideo extends TabContentView {
         backLayout = mAct.backLayout;
         friend_info = mAct.friend_info;
         init();
+        ShortVideoPublishManager.getInstance().setShortVideoUploadCallBack(new ShortVideoPublishManager.ShortVideoUploadCallBack() {
+            @Override
+            public void onSuccess() {
+
+            }
+
+            @Override
+            public void onProgress(int progress) {
+
+            }
+
+            @Override
+            public void onFailed() {
+
+            }
+        });
     }
 
     @Override
@@ -94,40 +111,73 @@ public class UserHomeVideo extends TabContentView {
         } else
             super.onResume(tag);
         theListView.setSelection(datas.isEmpty() ? 0 : 1);
+        if(ShortVideoPublishManager.getInstance().isUpload()){
+            //隐藏上传中item
+//            datas.remove(remove)
+        }
     }
 
     private void init() {
         // 结果显示
         loadManager = mAct.loadManager;
-        mEmptyContainer = (RelativeLayout) view.findViewById(R.id.empty_container);
-        mEmptyView = (LinearLayout) mEmptyContainer.findViewById(R.id.empty);
-        mGotoBtn = (Button) mEmptyView.findViewById(R.id.goto_btn);
+        mEmptyContainer =  view.findViewById(R.id.empty_container);
+        mEmptyView =  mEmptyContainer.findViewById(R.id.empty);
+        mGotoBtn =  mEmptyView.findViewById(R.id.goto_btn);
         mGotoBtn.setText("发视频");
-        mGotoBtn.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (LoginManager.isBindMobilePhone())
-                    mAct.startActivity(new Intent(mAct, VideoEditActivity.class));
-                else
-                    BaseLoginActivity.gotoBindPhoneNum(mAct);
-            }
+        mGotoBtn.setOnClickListener(v -> {
+            if (LoginManager.isBindMobilePhone())
+                mAct.startActivity(new Intent(mAct, VideoEditActivity.class));
+            else
+                BaseLoginActivity.gotoBindPhoneNum(mAct);
         });
-        theListView = (DownRefreshList) view.findViewById(R.id.list_myself_subject);
+        theListView = view.findViewById(R.id.list_myself_subject);
         theListView.setDivider(null);
         datas = new ArrayList<>();
-        mSecondEditDatas = new HashMap<String, Map<String, String>>();
-        mLocalDatas = new ArrayList<Map<String, String>>();
-        mNetDatas = new ArrayList<Map<String, String>>();
+        mSecondEditDatas = new HashMap<>();
+        mLocalDatas = new ArrayList<>();
+        mNetDatas = new ArrayList<>();
         adapter = new AdapterUserVideo(mAct, theListView, datas, 0, null, null);
         adapter.scaleType = ScaleType.CENTER_CROP;
         adapter.isAnimate = true;
-        adapter.setOnItemClickListener(new UserHomeItem.OnItemClickListener() {
-            @Override
-            public void onItemClick(UserHomeItem itemView, Map<String, String> dataMap) {
-                if (mOnItemClickListener != null)
-                    mOnItemClickListener.onItemClick(itemView, dataMap);
+        adapter.setOnItemClickListener((itemView, dataMap) -> {
+            if (mOnItemClickListener != null){
+                mOnItemClickListener.onItemClick(itemView, dataMap);
             }
         });
+        adapter.setOnDeleteClickCallback(this::showSureDelateDialog);
+    }
+
+    private void showSureDelateDialog(final Map<String, String> data) {
+        final DialogManager dialogManager = new DialogManager(mAct);
+        ViewManager viewManager = new ViewManager(dialogManager);
+        viewManager.setView(new TitleMessageView(mAct).setText("确认删除？"))
+                .setView(new HButtonView(mAct)
+                        .setNegativeText("否", v -> dialogManager.cancel())
+                        .setPositiveText("是", v -> {
+                            dialogManager.cancel();
+                            deleteData(data);
+                        }));
+        dialogManager.createDialog(viewManager).show();
+    }
+
+    private void deleteData(Map<String, String> data) {
+        if(data == null || data.isEmpty()){
+            return;
+        }
+        final String id  = data.get("id");
+        String fromLocal = data.get("dataFrom");
+        if("1".equals(fromLocal)){
+            //delete local
+            UploadVideoSQLite sqLite = new UploadVideoSQLite(mAct);
+            boolean isDeleted = sqLite.deleteById(Tools.parseIntOfThrow(id,-1));
+            Toast.makeText(mAct,isDeleted?"删除成功":"删除失败",Toast.LENGTH_SHORT).show();
+            if(isDeleted){
+                datas.remove(data);
+                adapter.notifyDataSetChanged();
+            }
+        }else if("2".equals(fromLocal)){
+            //delete remote
+        }
     }
 
     private void showDialog(final String text, final String url) {
@@ -210,65 +260,62 @@ public class UserHomeVideo extends TabContentView {
                 mLocalDatas.clear();
             if (mSecondEditDatas != null)
                 mSecondEditDatas.clear();
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    UploadVideoSQLite articleSQLite = new UploadVideoSQLite(mAct);
-                    ArrayList<UploadArticleData> articleDatas = articleSQLite.getAllUploadIngData();
-                    if (articleDatas != null && articleDatas.size() > 0) {
-                        for (UploadArticleData articleData : articleDatas) {
-                            if (articleData != null) {
-                                Map<String, String> data = new HashMap<String, String>();
-                                String code = articleData.getCode();
-                                String title = articleData.getTitle();
-                                String classCode = articleData.getClassCode();
-                                String content = articleData.getContent();
-                                int isOriginal = articleData.getIsOriginal();
-                                String repAddress = articleData.getRepAddress();
-                                String img = articleData.getImg();
-                                String imgs = articleData.getImgs();
-                                String videos = articleData.getVideos();
-                                String video = "";
-                                String videoUrl = "";
-                                String videoImgUrl = "";
-                                ArrayList<Map<String,String>> videoArray = articleData.getVideoArray();
-                                if(videoArray.size() > 0){
-                                    video = videoArray.get(0).get("video");
-                                    videoUrl = videoArray.get(0).get("videoUrl");
-                                    videoImgUrl = videoArray.get(0).get("imageUrl");
-                                }
-                                String uploadType = articleData.getUploadType();
-                                if (!TextUtils.isEmpty(uploadType) && UploadDishData.UPLOAD_ING_BACK.equals(uploadType))
-                                    continue;
-                                int id = articleData.getId();
-                                boolean hasMedia = articleSQLite.checkHasMedia(id);
-                                data.put("hasMedia", hasMedia ? "2" : "1");
-                                data.put("id", String.valueOf(id));
-                                data.put("code", code);
-                                data.put("title", title);
-                                data.put("classCode", classCode);
-                                data.put("content", content);
-                                data.put("isOriginal", String.valueOf(isOriginal));
-                                data.put("repAddress", repAddress);
-                                data.put("imgPath", img);
-                                data.put("video", video);
-                                data.put("videoUrl", videoUrl);
-                                data.put("videoImgUrl", videoImgUrl);
-                                data.put("uploadType", uploadType);
-                                data.put("imgs", imgs);
-                                data.put("videos", videos);
-                                data.put("isMe", "2");
-                                data.put("dataFrom", String.valueOf(1));//dataFrom:数据来源，本地:1；网络:2,或者null、""、不存在该字段；
-                                if (!TextUtils.isEmpty(code)) {
-                                    mSecondEditDatas.put(code, data);
-                                    continue;
-                                }
-                                mLocalDatas.add(data);
+            new Thread(() -> {
+                UploadVideoSQLite articleSQLite = new UploadVideoSQLite(mAct);
+                ArrayList<UploadArticleData> articleDatas = articleSQLite.getAllUploadIngData();
+                if (articleDatas != null && articleDatas.size() > 0) {
+                    for (UploadArticleData articleData : articleDatas) {
+                        if (articleData != null) {
+                            Map<String, String> data = new HashMap<String, String>();
+                            String code = articleData.getCode();
+                            String title = articleData.getTitle();
+                            String classCode = articleData.getClassCode();
+                            String content = articleData.getContent();
+                            int isOriginal = articleData.getIsOriginal();
+                            String repAddress = articleData.getRepAddress();
+                            String img = articleData.getImg();
+                            String imgs = articleData.getImgs();
+                            String videos = articleData.getVideos();
+                            String video = "";
+                            String videoUrl = "";
+                            String videoImgUrl = "";
+                            ArrayList<Map<String,String>> videoArray = articleData.getVideoArray();
+                            if(videoArray.size() > 0){
+                                video = videoArray.get(0).get("video");
+                                videoUrl = videoArray.get(0).get("videoUrl");
+                                videoImgUrl = videoArray.get(0).get("imageUrl");
                             }
+                            String uploadType = articleData.getUploadType();
+                            if (!TextUtils.isEmpty(uploadType) && UploadDishData.UPLOAD_ING_BACK.equals(uploadType))
+                                continue;
+                            int id = articleData.getId();
+                            boolean hasMedia = articleSQLite.checkHasMedia(id);
+                            data.put("hasMedia", hasMedia ? "2" : "1");
+                            data.put("id", String.valueOf(id));
+                            data.put("code", code);
+                            data.put("title", title);
+                            data.put("classCode", classCode);
+                            data.put("content", content);
+                            data.put("isOriginal", String.valueOf(isOriginal));
+                            data.put("repAddress", repAddress);
+                            data.put("imgPath", img);
+                            data.put("video", video);
+                            data.put("videoUrl", videoUrl);
+                            data.put("videoImgUrl", videoImgUrl);
+                            data.put("uploadType", uploadType);
+                            data.put("imgs", imgs);
+                            data.put("videos", videos);
+                            data.put("isMe", "2");
+                            data.put("dataFrom", String.valueOf(1));//dataFrom:数据来源，本地:1；网络:2,或者null、""、不存在该字段；
+                            if (!TextUtils.isEmpty(code)) {
+                                mSecondEditDatas.put(code, data);
+                                continue;
+                            }
+                            mLocalDatas.add(data);
                         }
                     }
-                    onDataReady(false);
                 }
+                onDataReady(false);
             }).start();
         }
 
@@ -293,42 +340,39 @@ public class UserHomeVideo extends TabContentView {
     }
 
     private void onDataReady(final boolean fromNet) {
-        mAct.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                if (!fromNet) {
-                    mLocalDataReady = true;
-                } else {
-                    mNetDataReady = true;
-                }
-                if ((!mLocalDataReady || !mNetDataReady) && isMyselft)
-                    return;
-                if (isRefresh) {
-                    datas.clear();
-                    datas.addAll(mLocalDatas);
-                    isRefresh = false;
-                }
-                if (!mSecondEditDatas.isEmpty() && !mNetDatas.isEmpty()) {
-                    ListIterator<Map<String, String>> netDatas = mNetDatas.listIterator();
-                    while(netDatas.hasNext()) {
-                        Map<String, String> netData = netDatas.next();
-                        String code = netData.get("code");
-                        if (!TextUtils.isEmpty(code) && mSecondEditDatas.size() > 0 && mSecondEditDatas.containsKey(code)) {
-                            netDatas.set(mSecondEditDatas.get(code));
-                            mSecondEditDatas.remove(code);
-                        }
+        mAct.runOnUiThread(() -> {
+            if (!fromNet) {
+                mLocalDataReady = true;
+            } else {
+                mNetDataReady = true;
+            }
+            if ((!mLocalDataReady || !mNetDataReady) && isMyselft)
+                return;
+            if (isRefresh) {
+                datas.clear();
+                datas.addAll(mLocalDatas);
+                isRefresh = false;
+            }
+            if (!mSecondEditDatas.isEmpty() && !mNetDatas.isEmpty()) {
+                ListIterator<Map<String, String>> netDatas = mNetDatas.listIterator();
+                while(netDatas.hasNext()) {
+                    Map<String, String> netData = netDatas.next();
+                    String code = netData.get("code");
+                    if (!TextUtils.isEmpty(code) && mSecondEditDatas.size() > 0 && mSecondEditDatas.containsKey(code)) {
+                        netDatas.set(mSecondEditDatas.get(code));
+                        mSecondEditDatas.remove(code);
                     }
                 }
-                datas.addAll(mNetDatas);
-                if (datas.size() == 0 && isMyselft) {
-                    RelativeLayout.LayoutParams emptyParams = (RelativeLayout.LayoutParams) mEmptyView.getLayoutParams();
-                    emptyParams.topMargin = mHeadViewHeight;
-                    mEmptyContainer.setVisibility(View.VISIBLE);
-                } else {
-                    mEmptyContainer.setVisibility(View.GONE);
-                    adapter.notifyDataSetChanged();
-                    theListView.setVisibility(View.VISIBLE);
-                }
+            }
+            datas.addAll(mNetDatas);
+            if (datas.size() == 0 && isMyselft) {
+                RelativeLayout.LayoutParams emptyParams = (RelativeLayout.LayoutParams) mEmptyView.getLayoutParams();
+                emptyParams.topMargin = mHeadViewHeight;
+                mEmptyContainer.setVisibility(View.VISIBLE);
+            } else {
+                mEmptyContainer.setVisibility(View.GONE);
+                adapter.notifyDataSetChanged();
+                theListView.setVisibility(View.VISIBLE);
             }
         });
     }
@@ -337,7 +381,7 @@ public class UserHomeVideo extends TabContentView {
      * 解析数据
      * @param returnObj 要解析的数据
      */
-    public int parseInfo(Object returnObj) {
+    private int parseInfo(Object returnObj) {
         if (mNetDatas != null && mNetDatas.size() > 0)
             mNetDatas.clear();
         int loadCount = 0;
--- app/src/main/java/amodule/user/view/UserHomeVideoItem.java
+++ app/src/main/java/amodule/user/view/UserHomeVideoItem.java
@@ -1,15 +1,31 @@
 package amodule.user.view;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.view.View;
 
+import com.bumptech.glide.Glide;
+import com.xiangha.R;
+
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Map;
+
+import acore.tools.StringManager;
+import amodule.dish.db.UploadDishData;
+
+import static amodule.dish.db.UploadDishData.UPLOAD_DRAF;
+import static amodule.dish.db.UploadDishData.UPLOAD_FAIL;
+import static amodule.dish.db.UploadDishData.UPLOAD_PAUSE;
 
 /**
  * Created by sll on 2017/5/24.
  */
 
 public class UserHomeVideoItem extends UserHomeTxtItem {
+    private String mUploadType = "";
     public UserHomeVideoItem(Context context) {
         super(context);
     }
@@ -30,6 +46,53 @@ public class UserHomeVideoItem extends UserHomeTxtItem {
     @Override
     public void setData(Map<String, String> dataMap, int position) {
         super.setData(dataMap, position);
+        boolean hasImg = false;
+        boolean hasVideo = false;
+        String fromLocal = mDataMap.get("dataFrom");
+        if ("1".equals(fromLocal)) {//dataFrom:数据来源，本地:1；网络:2,或者null、""、不存在该字段；
+            mUploadType = mDataMap.get("uploadType");
+        mUploadType = UPLOAD_FAIL;
+            String statusInfo = "";
+            if (mStatusInfo != null) {
+                switch (mUploadType) {
+                    case UPLOAD_FAIL:
+                        statusInfo = "上传失败，请重试";
+                        mStatusInfo.setTextColor(Color.parseColor("#f23030"));
+                        break;
+                    case UPLOAD_DRAF:
+                        statusInfo = "草稿箱";
+                        mStatusInfo.setTextColor(Color.parseColor("#999999"));
+                        break;
+                }
+            }
+            if (!TextUtils.isEmpty(statusInfo)) {
+                mStatusInfo.setText(statusInfo);
+                mStatusInfo.setVisibility(VISIBLE);
+                //TODO
+                mNameGourmet.setVisibility(GONE);
+                mNum1.setVisibility(GONE);
+                mNum2.setVisibility(GONE);
+            }
+            String path = null;
+            ArrayList<Map<String, String>> imgs = StringManager.getListMapByJson(mDataMap.get("imgs"));
+            ArrayList<Map<String, String>> videos = StringManager.getListMapByJson(mDataMap.get("videos"));
+            if (imgs != null && imgs.size() > 0) {
+                path = imgs.get(0).get("path");
+                hasImg = true;
+            } else if (videos != null && videos.size() > 0) {
+                hasVideo = true;
+                path = videos.get(0).get("image");
+            }
+            if (mImg != null && path != null) {
+                hasImg = true;
+                Glide.with(getContext())
+                        .load(new File(path))
+                        .override(getResources().getDimensionPixelSize(R.dimen.dp_110), getResources().getDimensionPixelSize(R.dimen.dp_72_5))
+                        .centerCrop()
+                        .into(mImg);
+                mImg.setVisibility(View.VISIBLE);
+            }
+        }
     }
 
     @Override
@@ -47,5 +110,7 @@ public class UserHomeVideoItem extends UserHomeTxtItem {
     protected void resetView() {
         super.resetView();
     }
+
+
 
 }
