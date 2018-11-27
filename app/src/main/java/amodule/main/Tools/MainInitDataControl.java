4aa11f8b26a271827ce9946c4fbcf82cf85f621f
mrtrying <30665145@qq.com>
2018-11-27 11:22:56 +0800

    Revert "替换filemanager"
    This reverts commit 9306f8e

--- app/src/main/java/amodule/main/Tools/MainInitDataControl.java
+++ app/src/main/java/amodule/main/Tools/MainInitDataControl.java
@@ -26,6 +26,7 @@ import org.eclipse.jetty.util.StringUtil;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -91,35 +92,38 @@ public class MainInitDataControl {
     /**
      * welcome之前初始化
      */
-    public void initWelcomeBefore(Context context){
+    public void initWelcomeBefore(Context context) {
 
     }
+
     /**
      * welcome oncreate初始化
      */
-    public void initWelcomeOncreate(){
-        Log.i("zhangyujian","initWelcomeOncreate");
-        long startTime= System.currentTimeMillis();
+    public void initWelcomeOncreate() {
+        Log.i("zhangyujian", "initWelcomeOncreate");
+        long startTime = System.currentTimeMillis();
         new Thread(new Runnable() {
             @Override
             public void run() {
-                try{
+                try {
                     CookieManager.getInstance().removeAllCookie();
-                }catch (Exception ignored){}
+                } catch (Exception ignored) {
+                }
                 XHClick.saveFirstStartTime(XHApplication.in());
                 XHClick.registerMonthSuperProperty(XHApplication.in());
             }
         }).start();
-        long endTime=System.currentTimeMillis();
-        Log.i("zhangyujian","initWelcomeOncreate::时间:"+(endTime-startTime));
+        long endTime = System.currentTimeMillis();
+        Log.i("zhangyujian", "initWelcomeOncreate::时间:" + (endTime - startTime));
 
     }
+
     /**
      * welcome布局完成之后之后初始化
      */
-    public void initWelcomeAfter(final Activity activity){
-        Log.i("zhangyujian","initWelcomeAfter");
-        long startTime= System.currentTimeMillis();
+    public void initWelcomeAfter(final Activity activity) {
+        Log.i("zhangyujian", "initWelcomeAfter");
+        long startTime = System.currentTimeMillis();
         initWelcome(activity);
         new Thread() {
             @Override
@@ -143,32 +147,35 @@ public class MainInitDataControl {
             }
         });
         ServiceManager.startProtectService(XHApplication.in());
-        long endTime2=System.currentTimeMillis();
-        Log.i("zhangyujian","initWelcomeAfter::时间:"+(endTime2-startTime));
+        long endTime2 = System.currentTimeMillis();
+        Log.i("zhangyujian", "initWelcomeAfter::时间:" + (endTime2 - startTime));
 
     }
+
     /**
      * Main之后初始化
      */
-    public void initMainBefore(){
+    public void initMainBefore() {
 
     }
+
     /**
      * Main oncreate初始化
      */
-    public void initMainOncreate(){
+    public void initMainOncreate() {
 
     }
+
     /**
      * Main之后初始化
      */
-    public void iniMainAfter(final Activity act){
-        Log.i("zhangyujian","iniMainAfter");
-        long startTime= System.currentTimeMillis();
+    public void iniMainAfter(final Activity act) {
+        Log.i("zhangyujian", "iniMainAfter");
+        long startTime = System.currentTimeMillis();
         //初始化语音
         new Thread(() -> {
             //讯飞语音： 请勿在“=”与 appid 之间添加任务空字符或者转义符
-            SpeechUtility.createUtility(act, SpeechConstant.APPID +"=56ce9191");
+            SpeechUtility.createUtility(act, SpeechConstant.APPID + "=56ce9191");
         }).start();
         // 发送页面存活时间
         XHClick.sendLiveTime(act);
@@ -176,7 +183,7 @@ public class MainInitDataControl {
         MallCommon.getDsInfo(act, null);
         //请求广告位
         AdConfigTools.getInstance().getAdConfigInfo();
-        long endTime=System.currentTimeMillis();
+        long endTime = System.currentTimeMillis();
         //七鱼初始化 init方法无需放入主进程中执行，其他的初始化，有必要放在放入主进程
         QiYvHelper.getInstance().initSDK(act);
 
@@ -185,7 +192,7 @@ public class MainInitDataControl {
 
         PollingConfig.COURSE_GUIDANCE.registerIHandleMessage(mIHandleMessage);
         AppHandlerAsyncPolling.getInstance().startPollingImmediately(PollingConfig.COURSE_GUIDANCE);
-        Log.i("zhangyujian","iniMainAfter::时间:"+(endTime-startTime));
+        Log.i("zhangyujian", "iniMainAfter::时间:" + (endTime - startTime));
     }
 
     public void setIHandleMessage(IHandleMessage IHandleMessage) {
@@ -202,16 +209,18 @@ public class MainInitDataControl {
             manager.addXGTag(XGTagManager.OFFICIAL);
         }
     }
+
     /**
      * main在界面展示后初始化
+     *
      * @param act
      */
-    public void initMainOnResume(final Activity act){
-        Log.i("zhangyujian","initMainOnResume");
+    public void initMainOnResume(final Activity act) {
+        Log.i("zhangyujian", "initMainOnResume");
 
         LoginManager.initYiYuanBindState(act, null);
 
-        long startTime= System.currentTimeMillis();
+        long startTime = System.currentTimeMillis();
 
         //更模版
         new XHTemplateManager().checkUplateAllTemplate();
@@ -233,15 +242,14 @@ public class MainInitDataControl {
 
         //获取随机推广数据
         AppCommon.saveRandPromotionData(act);
-        if(act!=null && XHADView.getInstence(act)!=null) {
+        if (act != null && XHADView.getInstence(act) != null) {
             XHADView.getInstence(act).setCanShowCallback(() -> Main.allMain != null
                     && Main.allMain.getCurrentTab() == 0);
         }
 
-        //判断旧数据库是否存在
-        String path = act.getDatabasePath("fullsrceen.db").getPath();
-        if (!TextUtils.isEmpty(path)){
-            act.deleteDatabase("fullsrceen.db");
+        //判断弹屏旧数据库是否存在
+        if (act != null && act.getDatabasePath("fullsrceen.db").exists()) {
+            UtilFile.delDirectoryOrFile(act.getDatabasePath("fullsrceen.db").getPath());
         }
 
         new AllPopDialogHelper(act).start();
@@ -250,11 +258,13 @@ public class MainInitDataControl {
 
         onMainResumeStatics();
 
-        long endTime2=System.currentTimeMillis();
-        Log.i("zhangyujian","initMainOnResume::时间::3::"+(endTime2-startTime));
+        long endTime2 = System.currentTimeMillis();
+        Log.i("zhangyujian", "initMainOnResume::时间::3::" + (endTime2 - startTime));
     }
 
-    /** 页面展示后，发送需要统计的数据 */
+    /**
+     * 页面展示后，发送需要统计的数据
+     */
     private void onMainResumeStatics() {
         new Thread(() -> {
             Object userCountStatics = FileManager.loadShared(XHApplication.in(), FileManager.xmlFile_appInfo, "userCount");
@@ -270,10 +280,12 @@ public class MainInitDataControl {
         }).start();
     }
 
-    /** Welcome应用数据初始化 */
+    /**
+     * Welcome应用数据初始化
+     */
     private void initWelcome(final Context context) {
-        Log.i("zhangyujian","initWelcome");
-        long startTime= System.currentTimeMillis();
+        Log.i("zhangyujian", "initWelcome");
+        long startTime = System.currentTimeMillis();
 
         // 自动登录
         MessageTipController.newInstance().getCommonData(null);
@@ -282,10 +294,10 @@ public class MainInitDataControl {
 
         AppCommon.clearCache();
 
-        long endTime4=System.currentTimeMillis();
-        Log.i("zhangyujian","initWelcome::时间:::3:"+(endTime4-startTime));
+        long endTime4 = System.currentTimeMillis();
+        Log.i("zhangyujian", "initWelcome::时间:::3:" + (endTime4 - startTime));
 
-        new Thread(){
+        new Thread() {
             @Override
             public void run() {
                 super.run();
@@ -305,18 +317,20 @@ public class MainInitDataControl {
                 //清除上传中的数据库数据
                 SubjectSqlite subjectSqlite = SubjectSqlite.getInstance(context);
                 ArrayList<SubjectData> array = subjectSqlite.selectByState(SubjectData.UPLOAD_ING);
-                for(SubjectData data : array){
+                for (SubjectData data : array) {
                     subjectSqlite.deleteById(data.getId());
                 }
-                FileManager.saveShared(context,FileManager.SHOW_NO_WIFI,FileManager.SHOW_NO_WIFI,"0");
+                FileManager.saveShared(context, FileManager.SHOW_NO_WIFI, FileManager.SHOW_NO_WIFI, "0");
             }
         }.start();
 
     }
 
-    /** 老版兼容问题 */
+    /**
+     * 老版兼容问题
+     */
     private void compatibleData(final Context context) {
-        new Thread(){
+        new Thread() {
             @Override
             public void run() {
                 super.run();
@@ -329,7 +343,7 @@ public class MainInitDataControl {
                 final String json = UtilFile.readFile(UtilFile.getDataDir() + FileManager.file_buyBurden);
                 if (json.length() > 0) {
                     new Thread(() -> {
-                        saveDataInDB(json,context);
+                        saveDataInDB(json, context);
                         UtilFile.delDirectoryOrFile(UtilFile.getDataDir() + FileManager.file_buyBurden);
                     }).start();
                 }
@@ -357,9 +371,10 @@ public class MainInitDataControl {
 
     /**
      * 处理数据,这里的context 再进行校验
+     *
      * @param json
      */
-    private void saveDataInDB(String json,Context context) {
+    private void saveDataInDB(String json, Context context) {
         DishOffData buyData = new DishOffData();
         ShowBuySqlite sqlite = new ShowBuySqlite(context);
         ArrayList<Map<String, String>> arrayList = getListMapByJson(json);
@@ -383,17 +398,17 @@ public class MainInitDataControl {
         sqlite.close();
     }
 
-    private void delayedExcute(@NonNull Runnable runnable){
-        if(runnable == null) return;
-        new Handler(Looper.getMainLooper()).postDelayed(runnable,delayedTime);
+    private void delayedExcute(@NonNull Runnable runnable) {
+        if (runnable == null) return;
+        new Handler(Looper.getMainLooper()).postDelayed(runnable, delayedTime);
     }
 
-    public void mainAfterUpload(Activity activity){
+    public void mainAfterUpload(Activity activity) {
         if (showQAUploading(activity))
             return;
-        if (showUploading(activity,new UploadArticleSQLite(XHApplication.in().getApplicationContext()), EditParentActivity.DATA_TYPE_ARTICLE, "您的文章还未上传完毕，是否继续上传？"))
+        if (showUploading(activity, new UploadArticleSQLite(XHApplication.in().getApplicationContext()), EditParentActivity.DATA_TYPE_ARTICLE, "您的文章还未上传完毕，是否继续上传？"))
             return;
-        if(showUploadingVideo(activity)){
+        if (showUploadingVideo(activity)) {
             return;
         }
     }
@@ -461,7 +476,8 @@ public class MainInitDataControl {
         }
         return show;
     }
-    private boolean showUploading(Activity activity,final UploadParentSQLite sqLite, final int dataType, String title) {
+
+    private boolean showUploading(Activity activity, final UploadParentSQLite sqLite, final int dataType, String title) {
         final UploadArticleData uploadArticleData = sqLite.getUploadIngData();
         if (uploadArticleData != null) {
             final DialogManager dialogManager = new DialogManager(activity);
@@ -471,7 +487,7 @@ public class MainInitDataControl {
                             .setNegativeText("取消", v -> {
                                 dialogManager.cancel();
                                 uploadArticleData.setUploadType(UploadDishData.UPLOAD_PAUSE);
-                                sqLite.update(uploadArticleData.getId(),uploadArticleData);
+                                sqLite.update(uploadArticleData.getId(), uploadArticleData);
                             })
                             .setPositiveTextColor(Color.parseColor("#007aff"))
                             .setPositiveText("确定", v -> {
@@ -480,8 +496,8 @@ public class MainInitDataControl {
                                 intent.putExtra("dataType", dataType);
                                 intent.putExtra("coverPath", uploadArticleData.getImg());
                                 String videoPath = "";
-                                ArrayList<Map<String,String>> videoArray = uploadArticleData.getVideoArray();
-                                if(videoArray.size() > 0){
+                                ArrayList<Map<String, String>> videoArray = uploadArticleData.getVideoArray();
+                                if (videoArray.size() > 0) {
                                     videoPath = videoArray.get(0).get("video");
                                 }
                                 intent.putExtra("finalVideoPath", videoPath);
@@ -492,15 +508,16 @@ public class MainInitDataControl {
         }
         return false;
     }
+
     private boolean showUploadingVideo(final Context act) {
         UploadVideoSQLite sqLite = new UploadVideoSQLite(act);
         final int uploadingId = sqLite.hasUploading();
-        if(uploadingId != -1){
-            if(sqLite.checkOver(UploadDishData.UPLOAD_FAIL)){
+        if (uploadingId != -1) {
+            if (sqLite.checkOver(UploadDishData.UPLOAD_FAIL)) {
                 sqLite.deleteById(uploadingId);
                 return false;
-            }else {
-                sqLite.update(uploadingId,UploadDishData.UPLOAD_FAIL);
+            } else {
+                sqLite.update(uploadingId, UploadDishData.UPLOAD_FAIL);
                 Toast.makeText(act, "您有上传失败的作品，已保存至个人主页", Toast.LENGTH_SHORT).show();
                 return true;
             }
