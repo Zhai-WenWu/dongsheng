a513f872a1e188d45e947bd0f85f44282acec483
zhaiwenwu <zhaiwenwu@xiangha.com>
2018-12-04 20:52:16 +0800

    学习界面ui搭建

--- app/src/main/java/amodule/lesson/activity/CourseDetail.java
+++ app/src/main/java/amodule/lesson/activity/CourseDetail.java
@@ -3,6 +3,10 @@ package amodule.lesson.activity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.RelativeLayout;
 
 import com.xiangha.R;
@@ -13,11 +17,11 @@ import java.util.Map;
 import acore.logic.load.LoadManager;
 import acore.override.activity.base.BaseAppCompatActivity;
 import acore.tools.StringManager;
+import acore.tools.Tools;
 import acore.widget.rvlistview.RvListView;
 import amodule.lesson.adapter.CourseVideoContentAdapter;
 import amodule.lesson.view.CourseDetailAskView;
 import amodule.lesson.view.CourseDetailIntroductionView;
-import amodule.lesson.view.CourseDetailClassCardView;
 import amodule.lesson.view.CourseDetailRadioButtonView;
 import amodule.lesson.view.CourseDetailTitleView;
 
@@ -31,6 +35,8 @@ public class CourseDetail extends BaseAppCompatActivity {
 
     private Map<String, String> mInfoMap;
     private RvListView mCourseList;
+    private CourseDetailRadioButtonView radioButtonMotionless;
+    private int titleHeight;
     private CourseDetailRadioButtonView radioButtonMove;
 
     @Override
@@ -42,22 +48,15 @@ public class CourseDetail extends BaseAppCompatActivity {
     }
 
     private void initView() {
-
         loadManager = new LoadManager(this, rl);
         rl = (RelativeLayout) findViewById(R.id.activityLayout);
         mCourseList = findViewById(R.id.course_list);
-        mCourseList.setAdapter(new CourseVideoContentAdapter(this,new ArrayList()));
-        radioButtonMove = findViewById(R.id.radiobutton_move);
-
-        mCourseList.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                Log.i("zww","onScrolled");
-
-            }
-        });
-
+        ArrayList<String> strings = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            strings.add("i");
+        }
+        mCourseList.setAdapter(new CourseVideoContentAdapter(this, strings));
+        radioButtonMotionless = findViewById(R.id.radiobutton_move);
     }
 
     private void loadInfo() {
@@ -81,15 +80,14 @@ public class CourseDetail extends BaseAppCompatActivity {
     private void initData() {
         //标题
         CourseDetailTitleView courseDetailTitleView = new CourseDetailTitleView(this);
+        int height = Tools.getMeasureHeight(courseDetailTitleView);
         mCourseList.addHeaderView(courseDetailTitleView);
 
         //课程表
-        CourseDetailClassCardView courseDetailClassCardView = new CourseDetailClassCardView(this);
-        mCourseList.addHeaderView(courseDetailClassCardView);
+        // TODO: 2018/12/4
 
         //tab按钮
-        CourseDetailRadioButtonView radioButtonMotionless = new CourseDetailRadioButtonView(this);
-        mCourseList.addHeaderView(radioButtonMotionless);
+        initTabView();
 
         //简介
         Map<String, String> desc = StringManager.getFirstMap(mInfoMap.get("desc"));
@@ -101,6 +99,76 @@ public class CourseDetail extends BaseAppCompatActivity {
         //问答
         CourseDetailAskView courseDetailAskView = new CourseDetailAskView(this);
         mCourseList.addFooterView(courseDetailAskView);
+    }
+
+
+    private void initTabView() {
+        radioButtonMove = new CourseDetailRadioButtonView(this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(0, 40, 1.0f);
+        for (int i = 0; i < 3; i++) {
+            RadioButton radioButton = new RadioButton(this);
+            radioButton.setGravity(Gravity.CENTER);
+            radioButton.setId(i);
+            radioButton.setButtonDrawable(null);
+            radioButton.setText("btn" + i);
+            //@color/cuouse_radiobutton_textcolor
+            radioButton.setTextColor(getResources().getColor(R.color.cuouse_radiobutton_textcolor));
+            radioButton.setTextSize(16);
+            radioButton.setBackgroundColor(getResources().getColor(R.color.transparent));
+//            if (i == 0)
+//                radioButton.setChecked(true);
+            radioButtonMove.getView().addView(radioButton, lp);
+
+
+            RadioButton radioButton1 = new RadioButton(this);
+            radioButton1.setGravity(Gravity.CENTER);
+            radioButton1.setId(i);
+            radioButton1.setButtonDrawable(null);
+            radioButton1.setText("btn" + i);
+            radioButton1.setTextColor(getResources().getColor(R.color.cuouse_radiobutton_textcolor));
+            radioButton1.setTextSize(16);
+            radioButton1.setBackgroundColor(getResources().getColor(R.color.transparent));
+//            if (i == 0)
+//                radioButton1.setChecked(true);
+            radioButtonMotionless.getView().addView(radioButton1, lp);
+            RadioButton child = (RadioButton) radioButtonMove.getView().getChildAt(0);
+            child.setChecked(true);
+        }
+
+        mCourseList.addHeaderView(radioButtonMove);
+
+        radioButtonMotionless.addOnCheckedChangedListener(new CourseDetailRadioButtonView.OnCheckedChangedListener() {
+            @Override
+            public void onClickChange(int checkedId) {
+                radioButtonMove.setClickIndex(checkedId);
+            }
+        });
+
+        radioButtonMove.addOnCheckedChangedListener(new CourseDetailRadioButtonView.OnCheckedChangedListener() {
+            @Override
+            public void onClickChange(int checkedId) {
+                radioButtonMotionless.setClickIndex(checkedId);
+                // TODO: 2018/12/4 定位
+                Log.i("radioButtonMove", checkedId + "");
+            }
+        });
+
+        View emptyView = new View(this);
+        mCourseList.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                emptyView.scrollBy(0,dy);
+//                    Log.i("logi", "top = " + radioButtonMove.getTop() + "   scrollY = " + scrollY);
+                if (emptyView.getScrollY() > radioButtonMove.getTop()) {
+                    radioButtonMotionless.setVisibility(View.VISIBLE);
+                } else {
+                    radioButtonMotionless.setVisibility(View.GONE);
+                }
+
+            }
+        });
+
     }
 
 
--- app/src/main/java/amodule/lesson/view/CourseDetailRadioButtonView.java
+++ app/src/main/java/amodule/lesson/view/CourseDetailRadioButtonView.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 
 import com.xiangha.R;
@@ -13,6 +16,10 @@ import java.util.Map;
 import amodule.lesson.activity.CourseDetail;
 
 public class CourseDetailRadioButtonView extends RelativeLayout {
+
+    private View view;
+    private RadioGroup radioGroup;
+
     public CourseDetailRadioButtonView(Context context) {
         this(context, null);
     }
@@ -23,6 +30,39 @@ public class CourseDetailRadioButtonView extends RelativeLayout {
 
     public CourseDetailRadioButtonView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        LayoutInflater.from(context).inflate(R.layout.view_course_radiobutton, this, true);
+        view = LayoutInflater.from(context).inflate(R.layout.view_course_radiobutton, this, true);
+        initView();
+    }
+
+    public RadioGroup getView() {
+        return radioGroup;
+    }
+
+    public void setClickIndex(int index) {
+        RadioButton child = (RadioButton) view.findViewById(index);
+        child.setChecked(true);
+    }
+
+    private void initView() {
+        radioGroup = view.findViewById(R.id.radiogroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (onCheckedChangedListener!=null) {
+                    onCheckedChangedListener.onClickChange(checkedId);
+                }
+
+            }
+        });
+    }
+
+    public void addOnCheckedChangedListener(OnCheckedChangedListener onCheckedChangedListener) {
+        this.onCheckedChangedListener = onCheckedChangedListener;
+    }
+
+    public OnCheckedChangedListener onCheckedChangedListener;
+
+    public interface OnCheckedChangedListener {
+        void onClickChange(int checkedId);
     }
 }
