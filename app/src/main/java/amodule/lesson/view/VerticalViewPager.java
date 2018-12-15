5ed4091ce0a19c81e48764aaea3ec193f7faa03e
zhaiwenwu <zhaiwenwu@xiangha.com>
2018-12-15 09:51:47 +0800

    private处理

--- app/src/main/java/amodule/lesson/view/VerticalViewPager.java
+++ app/src/main/java/amodule/lesson/view/VerticalViewPager.java
@@ -3,22 +3,17 @@ package amodule.lesson.view;
 import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Scroller;
+
+import java.lang.reflect.Field;
 
 public class VerticalViewPager extends ViewPager {
 
     private double transform;
-    public int showPosition;
-    private float mDownPosX;
-    private float mDownPosY;
-    private float deltaY;
-    int dealtX = 0;
-    int dealtY = 0;
-    int lastX = 0;
-    int lastY = 0;
-    boolean intercepted;
+    private int showPosition;
 
     public int getShowPosition() {
         return showPosition;
@@ -35,6 +30,7 @@ public class VerticalViewPager extends ViewPager {
     }
 
     private void init() {
+        setScroller();
         setPageTransformer(true, new VerticalPageTransformer());
         setOverScrollMode(OVER_SCROLL_NEVER);
         addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
@@ -88,42 +84,86 @@ public class VerticalViewPager extends ViewPager {
         return ev;
     }
 
+    private int downX;
+    private int downY;
+    private int distanceX;
+    private int distanceY;
+    private boolean intercepted;
+    private int evY;
+    private int evX;
+
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         super.onInterceptTouchEvent(swapXY(ev));
-        int y = (int) ev.getX();
-        int x = (int) ev.getY();
+        swapXY(ev);
+        evX = (int) ev.getX();
+        evY = (int) ev.getY();
 
         switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                dealtX = x;
-                dealtY = y;
+                downX = evX;
+                downY = evY;
                 intercepted = false;
                 break;
             case MotionEvent.ACTION_MOVE:
-                lastX = Math.abs(x - dealtX);
-                lastY = Math.abs(y - dealtY);
-                int y2 = y - dealtY;
-                if (lastY > lastX && ((getShowPosition() == 0) || getShowPosition() == 1 && y2 > 0 && mOnWebScrollTop != null && mOnWebScrollTop.canScroll())) {
+                distanceX = Math.abs(evX - downX);
+                distanceY = Math.abs(evY - downY);
+                int distance = evY - downY;
+                if (distanceY > distanceX && ((getShowPosition() == 0) || getShowPosition() == 1 && distance > 0 && mOnWebScrollTop != null && mOnWebScrollTop.canScroll())) {
                     intercepted = true;
                 } else {
                     intercepted = false;
                 }
                 break;
         }
-        swapXY(ev);
         return intercepted;
     }
 
+    private int eventY;
+
     @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        return super.onTouchEvent(swapXY(ev));
+    public boolean onTouchEvent(MotionEvent event) {
+        eventY = (int) event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_UP:
+                if (Math.abs(downY - eventY) > 200) {
+                    if (getShowPosition() == 0) {
+                        setCurrentItem(1);
+                    } else if (getShowPosition() == 1) {
+                        setCurrentItem(0);
+                    }
+                    return true;
+                }
+        }
+        return super.onTouchEvent(swapXY(event));
     }
 
     private OnWebScrollTop mOnWebScrollTop;
 
     public void setWebScrollTop(OnWebScrollTop mOnWebScrollTop) {
         this.mOnWebScrollTop = mOnWebScrollTop;
+    }
+
+    private void setScroller() {
+        try {
+            Class<?> viewpager = ViewPager.class;
+            Field scroller = viewpager.getDeclaredField("mScroller");
+            scroller.setAccessible(true);
+            scroller.set(this, new ViewPagerScroller(getContext()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class ViewPagerScroller extends Scroller {
+        public ViewPagerScroller(Context context) {
+            super(context, new DecelerateInterpolator());
+        }
+
+        @Override
+        public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+            super.startScroll(startX, startY, dx, dy, 300);
+        }
     }
 
     public interface OnWebScrollTop {
@@ -138,6 +178,7 @@ public class VerticalViewPager extends ViewPager {
 
     public interface OnScrollDistance {
         void scrollDistance(float distance);
+
         void scrollEnd(int state);
     }
 
